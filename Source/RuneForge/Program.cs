eb9705966bcafa9cd8df4b1181da2363cc04bdb1
Andrey Talanin <andrey.talanin@outlook.com>
2021-12-11 14:51:44 +0300

    Create the RuneForge game project representing an OpenGL-based game

---
+++ Source/RuneForge/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.DependencyInjection;
+
+namespace RuneForge
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static async Task Main(string[] args)
+        {
+            using IHost host = CreateHostBuilder(args).ConfigureServices(ConfigureServices).Build();
+            using Game game = host.Services.GetRequiredService<Game>();
+
+            await host.StartAsync();
+            game.Run();
+
+            await host.StopAsync();
+        }
+
+        private static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args);
+        }
+
+        private static void ConfigureServices(HostBuilderContext context, IServiceCollection services)
+        {
+            services.AddMonoGame((serviceProvider, gameWindow) => new RuneForgeGame(serviceProvider, gameWindow));
+        }
+    }
+}
