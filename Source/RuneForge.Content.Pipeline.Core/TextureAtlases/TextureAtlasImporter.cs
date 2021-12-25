904ac14a3e63ad14ea3c068b71b65c99c32a6627
Andrey Talanin <andrey.talanin@outlook.com>
2021-12-25 20:00:37 +0300

    Add the TextureAtlasImporter content type importer class

---
+++ Source/RuneForge.Content.Pipeline.Core/TextureAtlases/TextureAtlasImporter.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+using Microsoft.Xna.Framework.Content.Pipeline;
+
+namespace RuneForge.Content.Pipeline.Core.TextureAtlases
+{
+    [ContentImporter(".xml", DisplayName = "Texture Atlas Importer - RuneForge", DefaultProcessor = "PassThroughProcessor")]
+    public class TextureAtlasImporter : ContentImporter<TextureAtlas>
+    {
+        public override TextureAtlas Import(string fileName, ContentImporterContext context)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(TextureAtlas));
+            using StreamReader reader = new StreamReader(fileName);
+            return (TextureAtlas)xmlSerializer.Deserialize(reader);
+        }
+    }
+}
