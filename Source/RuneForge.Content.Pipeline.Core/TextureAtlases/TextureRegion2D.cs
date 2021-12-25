b9b95404f58a7dd5f129d2d5c339618d4d71ce3f
Andrey Talanin <andrey.talanin@outlook.com>
2021-12-25 19:54:48 +0300

    Add the TextureRegion2D data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Core/TextureAtlases/TextureRegion2D.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Serialization;
+
+namespace RuneForge.Content.Pipeline.Core.TextureAtlases
+{
+    public class TextureRegion2D
+    {
+        [XmlAttribute(AttributeName = "Name")]
+        public string Name { get; set; }
+
+        [XmlAttribute(AttributeName = "X")]
+        public int X { get; set; }
+
+        [XmlAttribute(AttributeName = "Y")]
+        public int Y { get; set; }
+
+        [XmlAttribute(AttributeName = "Width")]
+        public int Width { get; set; }
+
+        [XmlAttribute(AttributeName = "Height")]
+        public int Height { get; set; }
+    }
+}
