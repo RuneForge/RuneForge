995ab3fb30e9d636ea68ab17311b92329e913988
Andrey Talanin <andrey.talanin@outlook.com>
2021-12-26 18:28:25 +0300

    Add the AnimationAtlas data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Core/AnimationAtlases/AnimationAtlas.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace RuneForge.Content.Pipeline.Core.AnimationAtlases
+{
+    [XmlRoot(ElementName = "AnimationAtlas")]
+    public class AnimationAtlas
+    {
+        [XmlElement(ElementName = "TextureAssetName")]
+        public string TextureAssetName { get; set; }
+
+        [XmlArray(ElementName = "Animations")]
+        [XmlArrayItem(ElementName = "Animation")]
+        public List<Animation2D> Animations { get; set; }
+    }
+}
