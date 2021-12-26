816d979426b7c0e9f41dd8d3a926a87b2ebd14d2
Andrey Talanin <andrey.talanin@outlook.com>
2021-12-26 18:41:38 +0300

    Add the AnimationAtlasImporter content type importer class

---
+++ Source/RuneForge.Content.Pipeline.Core/AnimationAtlases/AnimationAtlasImporter.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+using System.Xml.Serialization;
+
+using Microsoft.Xna.Framework.Content.Pipeline;
+
+namespace RuneForge.Content.Pipeline.Core.AnimationAtlases
+{
+    [ContentImporter(".xml", DisplayName = "Animation Atlas Importer - RuneForge", DefaultProcessor = "PassThroughProcessor")]
+    public class AnimationAtlasImporter : ContentImporter<AnimationAtlas>
+    {
+        public override AnimationAtlas Import(string fileName, ContentImporterContext context)
+        {
+            XmlSerializer xmlSerializer = new XmlSerializer(typeof(AnimationAtlas));
+            using StreamReader reader = new StreamReader(fileName);
+            return (AnimationAtlas)xmlSerializer.Deserialize(reader);
+        }
+    }
+}
