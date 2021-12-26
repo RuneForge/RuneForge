f5214dc5afdac9e87ec53269b57b500516b4bd3a
Andrey Talanin <andrey.talanin@outlook.com>
2021-12-26 18:14:23 +0300

    Add the AnimationAtlas class

---
+++ Source/RuneForge.Core/AnimationAtlases/AnimationAtlas.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RuneForge.Core.AnimationAtlases
+{
+    public class AnimationAtlas
+    {
+        public string Name { get; }
+
+        public Texture2D Texture { get; }
+
+        public IReadOnlyDictionary<string, Animation2D> Animations { get; }
+
+        public AnimationAtlas(string name, Texture2D texture, IDictionary<string, Animation2D> animations)
+        {
+            Name = !string.IsNullOrEmpty(name)
+                ? name : throw new ArgumentException("Name of the animation atlas can not be null or empty.", nameof(name));
+            Texture = texture ?? throw new ArgumentNullException(nameof(texture));
+            Animations = animations != null
+                ? new ReadOnlyDictionary<string, Animation2D>(animations) : throw new ArgumentNullException(nameof(animations));
+        }
+    }
+}
