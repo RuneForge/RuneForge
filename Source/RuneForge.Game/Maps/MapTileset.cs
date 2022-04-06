4d34fa75f6a765f9ffc2ce4a547aa1274c984c52
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-06 22:49:59 +0300

    Add the MapTileset class

---
+++ Source/RuneForge.Game/Maps/MapTileset.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace RuneForge.Game.Maps
+{
+    public class MapTileset
+    {
+        public string TextureAtlasName { get; }
+
+        public ReadOnlyDictionary<(MapCellTier, MapCellType), MapTilesetCellPrototype> CellPrototypes { get; }
+
+        public MapTileset(string textureAtlasName, IDictionary<(MapCellTier, MapCellType), MapTilesetCellPrototype> cellPrototypes)
+        {
+            TextureAtlasName = textureAtlasName;
+            CellPrototypes = new ReadOnlyDictionary<(MapCellTier, MapCellType), MapTilesetCellPrototype>(cellPrototypes);
+        }
+
+        public MapTilesetCellPrototype GetCellPrototype(MapCellTier tier, MapCellType type)
+        {
+            return CellPrototypes[(tier, type)];
+        }
+
+        public bool TryGetCellPrototype(MapCellTier tier, MapCellType type, out MapTilesetCellPrototype cellPrototype)
+        {
+            return CellPrototypes.TryGetValue((tier, type), out cellPrototype);
+        }
+    }
+}
