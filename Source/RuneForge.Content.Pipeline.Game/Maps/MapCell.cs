ddb505d0b636b174a42fba7b285cd93e8d33baaf
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-06 22:56:36 +0300

    Add the MapCell data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Maps/MapCell.cs
@@ -0,0 +1,14 @@
+﻿using System.Xml.Serialization;
+
+using RuneForge.Game.Maps;
+
+namespace RuneForge.Content.Pipeline.Game.Maps
+{
+    public class MapCell
+    {
+        [XmlAttribute(AttributeName = "Tier")]
+        public MapCellTier Tier { get; set; }
+        [XmlAttribute(AttributeName = "Type")]
+        public MapCellType Type { get; set; }
+    }
+}
