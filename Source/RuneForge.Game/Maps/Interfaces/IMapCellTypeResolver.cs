a8d3647ec01273913931c9d0f22fc45fd84b093b
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-09 21:29:32 +0300

    Add the MapCellTypeResolver service class

---
+++ Source/RuneForge.Game/Maps/Interfaces/IMapCellTypeResolver.cs
@@ -0,0 +1,9 @@
+﻿namespace RuneForge.Game.Maps.Interfaces
+{
+    public interface IMapCellTypeResolver
+    {
+        public MapCellType ResolveMapCellType(int x, int y, Map map);
+
+        public bool TryResolveMapCellType(int x, int y, Map map, out MapCellType type);
+    }
+}
