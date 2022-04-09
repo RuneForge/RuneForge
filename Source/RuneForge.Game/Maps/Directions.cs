96ff27dd819fc5fc4ca66c2fe364780023df0c7b
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-09 21:29:08 +0300

    Add the Directions flag enum

---
+++ Source/RuneForge.Game/Maps/Directions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace RuneForge.Game.Maps
+{
+    [Flags]
+    public enum Directions
+    {
+        None = 0x00,
+        North = 0x01,
+        South = 0x02,
+        West = 0x04,
+        East = 0x08,
+        NorthWest = 0x10,
+        NorthEast = 0x20,
+        SouthWest = 0x40,
+        SouthEast = 0x80,
+    }
+}
