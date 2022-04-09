a986824fbe0b457697edef0fdbdf759fcf7bad2a
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-10 00:19:49 +0300

    Add the SpriteBatchProvider service class

---
+++ Source/RuneForge.Core/Rendering/Interfaces/ISpriteBatchProvider.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+using Microsoft.Xna.Framework.Graphics;
+
+namespace RuneForge.Core.Rendering.Interfaces
+{
+    public interface ISpriteBatchProvider
+    {
+        public SpriteBatch WorldSpriteBatch { get; set; }
+    }
+}
