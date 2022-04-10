012890064562daf00b665f91ea5cb120bf67797a
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-10 18:49:15 +0300

    Add the MapTilesetDecorationPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Maps/MapTilesetDecorationPrototype.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+using RuneForge.Game.Maps;
+
+namespace RuneForge.Content.Pipeline.Game.Maps
+{
+    public class MapTilesetDecorationPrototype
+    {
+        [XmlAttribute(AttributeName = "Tier")]
+        public MapDecorationTier Tier { get; set; }
+        [XmlAttribute(AttributeName = "Type")]
+        public MapDecorationType Type { get; set; }
+
+        [XmlElement(ElementName = "TextureRegionName")]
+        public string TextureRegionName { get; set; }
+    }
+}
