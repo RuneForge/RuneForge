0bc61419fbec48afb8ea9823d5e2d7c2261f63ce
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-10 18:30:09 +0300

    Add the MapDecoration read-only struct

---
+++ Source/RuneForge.Game/Maps/MapDecoration.cs
@@ -0,0 +1,14 @@
+﻿namespace RuneForge.Game.Maps
+{
+    public struct MapDecoration
+    {
+        public MapDecorationTier Tier { get; }
+        public MapDecorationType Type { get; }
+
+        public MapDecoration(MapDecorationTier tier, MapDecorationType type)
+        {
+            Tier = tier;
+            Type = type;
+        }
+    }
+}
