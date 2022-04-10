9c591713df5ce22e4a0d78f5494c3e4f12e151a4
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-10 18:31:58 +0300

    Add map decoration prototypes to the MapTileset class

--- Source/RuneForge.Game/Maps/MapTileset.cs
+++ Source/RuneForge.Game/Maps/MapTileset.cs
@@ -8,21 +8,35 @@ namespace RuneForge.Game.Maps
         public string TextureAtlasName { get; }
 
         public ReadOnlyDictionary<(MapCellTier, MapCellType), MapTilesetCellPrototype> CellPrototypes { get; }
+        public ReadOnlyDictionary<(MapDecorationTier, MapDecorationType), MapTilesetDecorationPrototype> DecorationPrototypes { get; }
 
-        public MapTileset(string textureAtlasName, IDictionary<(MapCellTier, MapCellType), MapTilesetCellPrototype> cellPrototypes)
+        public MapTileset(
+            string textureAtlasName,
+            IDictionary<(MapCellTier, MapCellType), MapTilesetCellPrototype> cellPrototypes,
+            IDictionary<(MapDecorationTier, MapDecorationType), MapTilesetDecorationPrototype> decorationPrototypes
+            )
         {
             TextureAtlasName = textureAtlasName;
             CellPrototypes = new ReadOnlyDictionary<(MapCellTier, MapCellType), MapTilesetCellPrototype>(cellPrototypes);
+            DecorationPrototypes = new ReadOnlyDictionary<(MapDecorationTier, MapDecorationType), MapTilesetDecorationPrototype>(decorationPrototypes);
         }
 
         public MapTilesetCellPrototype GetCellPrototype(MapCellTier tier, MapCellType type)
         {
             return CellPrototypes[(tier, type)];
         }
+        public MapTilesetDecorationPrototype GetDecorationPrototype(MapDecorationTier tier, MapDecorationType type)
+        {
+            return DecorationPrototypes[(tier, type)];
+        }
 
         public bool TryGetCellPrototype(MapCellTier tier, MapCellType type, out MapTilesetCellPrototype cellPrototype)
         {
             return CellPrototypes.TryGetValue((tier, type), out cellPrototype);
+        }
+        public bool TryGetDecorationPrototype(MapDecorationTier tier, MapDecorationType type, out MapTilesetDecorationPrototype decorationPrototype)
+        {
+            return DecorationPrototypes.TryGetValue((tier, type), out decorationPrototype);
         }
     }
 }
