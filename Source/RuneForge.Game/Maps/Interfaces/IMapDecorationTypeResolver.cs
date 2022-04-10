cab6f7a6576886bca0999293664d0883d11362b9
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-10 19:57:51 +0300

    Add the MapDecorationTypeResolver service class

---
+++ Source/RuneForge.Game/Maps/Interfaces/IMapDecorationTypeResolver.cs
@@ -0,0 +1,9 @@
+﻿namespace RuneForge.Game.Maps.Interfaces
+{
+    public interface IMapDecorationTypeResolver
+    {
+        public MapDecorationType ResolveMapDecorationType(int x, int y, Map map);
+
+        public bool TryResolveMapDecorationType(int x, int y, Map map, out MapDecorationType type);
+    }
+}
