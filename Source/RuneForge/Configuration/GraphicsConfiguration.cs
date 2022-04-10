a9c649fe33ba1e25889c04ececf541e593217bc1
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-10 22:09:07 +0300

    Add the GraphicsConfiguration class representing a section of configuration

---
+++ Source/RuneForge/Configuration/GraphicsConfiguration.cs
@@ -0,0 +1,13 @@
+﻿namespace RuneForge.Configuration
+{
+    public class GraphicsConfiguration
+    {
+        public int BackBufferWidth { get; set; }
+
+        public int BackBufferHeight { get; set; }
+
+        public bool UseHardwareFullScreen { get; set; }
+
+        public bool UseFullScreen { get; set; }
+    }
+}
