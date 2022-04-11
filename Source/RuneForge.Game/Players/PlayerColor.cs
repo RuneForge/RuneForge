a93d29a5234042d3db35ea2a1bdea13553f765e3
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-11 13:24:58 +0300

    Add the PlayerColor struct

---
+++ Source/RuneForge.Game/Players/PlayerColor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace RuneForge.Game.Players
+{
+    public struct PlayerColor
+    {
+        public Color MainColor { get; }
+
+        public Color EntityColorShadeA { get; }
+        public Color EntityColorShadeB { get; }
+        public Color EntityColorShadeC { get; }
+        public Color EntityColorShadeD { get; }
+
+        public PlayerColor(Color mainColor, Color entityColorShadeA, Color entityColorShadeB, Color entityColorShadeC, Color entityColorShadeD)
+        {
+            MainColor = mainColor;
+            EntityColorShadeA = entityColorShadeA;
+            EntityColorShadeB = entityColorShadeB;
+            EntityColorShadeC = entityColorShadeC;
+            EntityColorShadeD = entityColorShadeD;
+        }
+    }
+}
