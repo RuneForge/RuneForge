e5aea1e6be69639814781d8240964982ad6cf4b1
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-11 16:22:58 +0300

    Add the InMemoryPlayerRepository repository class

---
+++ Source/RuneForge.Data/Players/Interfaces/IPlayerRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace RuneForge.Data.Players.Interfaces
+{
+    public interface IPlayerRepository
+    {
+        public PlayerDto GetPlayer(Guid playerId);
+
+        public ReadOnlyCollection<PlayerDto> GetPlayers();
+
+        public void AddPlayer(PlayerDto player);
+
+        public void SavePlayer(PlayerDto player);
+
+        public void RemovePlayer(Guid playerId);
+    }
+}
