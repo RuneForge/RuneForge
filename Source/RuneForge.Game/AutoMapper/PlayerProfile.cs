16512d103016983b6dc3a993a60436f76dae18a3
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-11 20:26:21 +0300

    Add the PlayerProfile AutoMapper profile class

---
+++ Source/RuneForge.Game/AutoMapper/PlayerProfile.cs
@@ -0,0 +1,29 @@
+﻿using AutoMapper;
+
+using RuneForge.Data.Players;
+using RuneForge.Game.Players;
+
+namespace RuneForge.Game.AutoMapper
+{
+    public class PlayerProfile : Profile
+    {
+        public PlayerProfile()
+        {
+            SourceMemberNamingConvention = new ExactMatchNamingConvention();
+            DestinationMemberNamingConvention = new ExactMatchNamingConvention();
+
+            CreateMap<Player, PlayerDto>();
+            CreateMap<PlayerDto, Player>().ConstructUsing((player, context) =>
+            {
+                PlayerColor playerColor = context.Mapper.Map<PlayerColorDto, PlayerColor>(player.Color);
+                return new Player(player.Id, player.Name, playerColor);
+            });
+
+            CreateMap<PlayerColor, PlayerColorDto>();
+            CreateMap<PlayerColorDto, PlayerColor>().ConstructUsing((playerColor, context) =>
+            {
+                return new PlayerColor(playerColor.MainColor, playerColor.EntityColorShadeA, playerColor.EntityColorShadeB, playerColor.EntityColorShadeC, playerColor.EntityColorShadeD);
+            });
+        }
+    }
+}
