23260373d8cd5fe8f869279ef51bc7e9375b2722
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-12 20:46:46 +0300

    Add the UnitDto data transfer class

---
+++ Source/RuneForge.Data/Units/UnitDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RuneForge.Data.Units
+{
+    public class UnitDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid OwnerId { get; set; }
+    }
+}
