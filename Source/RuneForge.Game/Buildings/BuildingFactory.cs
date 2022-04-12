ab1da3bbde9949622cd224b31bc7cb7af31a4ee4
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-12 20:59:59 +0300

    Add the BuildingFactory service class

---
+++ Source/RuneForge.Game/Buildings/BuildingFactory.cs
@@ -0,0 +1,23 @@
+﻿using RuneForge.Data.Buildings;
+using RuneForge.Game.Buildings.Interfaces;
+using RuneForge.Game.Players;
+using RuneForge.Game.Players.Interfaces;
+
+namespace RuneForge.Game.Buildings
+{
+    public class BuildingFactory : IBuildingFactory
+    {
+        private readonly IPlayerService m_playerService;
+
+        public BuildingFactory(IPlayerService playerService)
+        {
+            m_playerService = playerService;
+        }
+
+        public Building CreateFromDto(BuildingDto building)
+        {
+            Player owner = m_playerService.GetPlayer(building.OwnerId);
+            return new Building(building.Id, building.Name, owner);
+        }
+    }
+}
---
+++ Source/RuneForge.Game/Buildings/Interfaces/IBuildingFactory.cs
@@ -0,0 +1,9 @@
+﻿using RuneForge.Data.Buildings;
+
+namespace RuneForge.Game.Buildings.Interfaces
+{
+    public interface IBuildingFactory
+    {
+        public Building CreateFromDto(BuildingDto building);
+    }
+}
