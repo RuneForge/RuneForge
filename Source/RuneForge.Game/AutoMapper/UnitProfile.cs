6a6f36707089888f60ee254faef418a7752b3fdc
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-12 20:52:19 +0300

    Add the UnitProfile AutoMapper profile class

---
+++ Source/RuneForge.Game/AutoMapper/UnitProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+
+using RuneForge.Data.Units;
+using RuneForge.Game.Units;
+
+namespace RuneForge.Game.AutoMapper
+{
+    public class UnitProfile : Profile
+    {
+        public UnitProfile()
+        {
+            SourceMemberNamingConvention = new ExactMatchNamingConvention();
+            DestinationMemberNamingConvention = new ExactMatchNamingConvention();
+
+            CreateMap<Unit, UnitDto>()
+                .ForMember(unit => unit.OwnerId, options => options.MapFrom(unit => unit.Owner.Id));
+        }
+    }
+}
