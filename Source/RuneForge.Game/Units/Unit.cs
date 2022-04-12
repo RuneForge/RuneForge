385063ee9e5e8ee6034e209f543ab1f4eb564dd7
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-12 20:48:42 +0300

    Add the Unit class

---
+++ Source/RuneForge.Game/Units/Unit.cs
@@ -0,0 +1,20 @@
+﻿using RuneForge.Game.Players;
+
+namespace RuneForge.Game.Units
+{
+    public class Unit
+    {
+        public int Id { get; }
+
+        public string Name { get; }
+
+        public Player Owner { get; }
+
+        public Unit(int id, string name, Player owner)
+        {
+            Id = id;
+            Name = name;
+            Owner = owner;
+        }
+    }
+}
