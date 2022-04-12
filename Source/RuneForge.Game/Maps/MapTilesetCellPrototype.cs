8776e9ded2b45040ee0537e6d8eebc1aa375b1f4
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-12 23:22:54 +0300

    Add movement and building flags to the MapTilesetCellPrototype class

--- Source/RuneForge.Game/Maps/MapTilesetCellPrototype.cs
+++ Source/RuneForge.Game/Maps/MapTilesetCellPrototype.cs
@@ -2,6 +2,16 @@
 {
     public class MapTilesetCellPrototype
     {
-        public string TextureRegionName { get; set; }
+        public MapCellMovementFlags MovementFlags { get; }
+        public MapCellBuildingFlags BuildingFlags { get; }
+
+        public string TextureRegionName { get; }
+
+        public MapTilesetCellPrototype(MapCellMovementFlags movementFlags, MapCellBuildingFlags buildingFlags, string textureRegionName)
+        {
+            MovementFlags = movementFlags;
+            BuildingFlags = buildingFlags;
+            TextureRegionName = textureRegionName;
+        }
     }
 }
