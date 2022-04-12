963d9cfd5716d1023c978fc424c6df0ef12988b2
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-12 20:58:17 +0300

    Add the BuildingDto data transfer class

---
+++ Source/RuneForge.Data/Buildings/BuildingDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RuneForge.Data.Buildings
+{
+    public class BuildingDto
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public Guid OwnerId { get; set; }
+    }
+}
