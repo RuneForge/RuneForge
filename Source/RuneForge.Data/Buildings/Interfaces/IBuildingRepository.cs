460c632d3c6412e49f0ff1c841c9c82556359987
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-12 20:58:35 +0300

    Add the InMemoryBuildingRepository repository class

---
+++ Source/RuneForge.Data/Buildings/Interfaces/IBuildingRepository.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.ObjectModel;
+
+namespace RuneForge.Data.Buildings.Interfaces
+{
+    public interface IBuildingRepository
+    {
+        public BuildingDto GetBuilding(int buildingId);
+
+        public ReadOnlyCollection<BuildingDto> GetBuildings();
+
+        public void AddBuilding(BuildingDto building);
+
+        public void SaveBuilding(BuildingDto building);
+
+        public void RemoveBuilding(int buildingId);
+    }
+}
