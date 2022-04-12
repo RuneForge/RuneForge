a6fbbdc1a97bc4fb47b1d04b615f9fb28d8b4287
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-12 22:32:43 +0300

    Add the MapCellBuildingFlags flag enum

---
+++ Source/RuneForge.Game/Maps/MapCellBuildingFlags.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RuneForge.Game.Maps
+{
+    [Flags]
+    public enum MapCellBuildingFlags
+    {
+        None = 0x00,
+        LandBuildingAllowed = 0x01,
+        CoastalBuildingAllowed = 0x02,
+        WaterBuildingAllowed = 0x04,
+    }
+}
