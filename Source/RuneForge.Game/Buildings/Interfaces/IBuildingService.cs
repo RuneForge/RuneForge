7f0969a78749bd23110b23efcbb281c9a1cd8f92
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-12 20:59:43 +0300

    Add the BuildingService service class

---
+++ Source/RuneForge.Game/Buildings/Interfaces/IBuildingService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+
+namespace RuneForge.Game.Buildings.Interfaces
+{
+    public interface IBuildingService
+    {
+        public Building GetBuilding(int buildingId);
+
+        public ReadOnlyCollection<Building> GetBuildings();
+
+        public void AddBuilding(Building building);
+
+        public void RemoveBuilding(int buildingId);
+    }
+}
