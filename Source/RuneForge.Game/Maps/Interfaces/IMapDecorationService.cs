7671e2ba6c4789b1568256ddca2140703f0a6175
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-13 17:10:46 +0300

    Add the MapDecorationService service class

---
+++ Source/RuneForge.Game/Maps/Interfaces/IMapDecorationService.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.ObjectModel;
+
+namespace RuneForge.Game.Maps.Interfaces
+{
+    public interface IMapDecorationService
+    {
+        public MapDecoration GetMapDecoration(int mapDecorationId);
+
+        public ReadOnlyCollection<MapDecoration> GetMapDecorations();
+
+        public void AddMapDecoration(MapDecoration mapDecoration);
+
+        public void RemoveMapDecoration(int mapDecorationId);
+    }
+}
