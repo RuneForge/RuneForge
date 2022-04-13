f2a232c262e7b317f3707917f86beb7abd65b556
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-13 15:05:00 +0300

    Add the MapDecorationPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Maps/MapDecorationPrototype.cs
@@ -0,0 +1,10 @@
+﻿using System.Xml.Serialization;
+
+namespace RuneForge.Content.Pipeline.Game.Maps
+{
+    public class MapDecorationPrototype
+    {
+        [XmlAttribute(AttributeName = "Name")]
+        public string Name { get; set; }
+    }
+}
