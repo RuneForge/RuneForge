8bf608baf174a521aac5574362a4f0056ed6fb78
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-13 14:28:41 +0300

    Add the MapDecorationFactory service class

---
+++ Source/RuneForge.Game/Maps/Interfaces/IMapDecorationFactory.cs
@@ -0,0 +1,7 @@
+﻿namespace RuneForge.Game.Maps.Interfaces
+{
+    public interface IMapDecorationFactory
+    {
+        public MapDecoration CreateFromPrototype(int x, int y, MapDecorationPrototype prototype);
+    }
+}
