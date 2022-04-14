f6eb183cec54d2ad3a342e33c77ad428e401c902
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-14 12:36:38 +0300

    Add the Player data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Players/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace RuneForge.Content.Pipeline.Game.Players
+{
+    public class Player
+    {
+        [XmlAttribute(AttributeName = "Id")]
+        public Guid Id { get; set; }
+
+        [XmlAttribute(AttributeName = "Name")]
+        public string Name { get; set; }
+
+        [XmlElement(ElementName = "Color")]
+        public PlayerColor Color { get; set; }
+    }
+}
