540349cafc25ec9b796309577f4b7dd111840ec7
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-14 16:43:18 +0300

    Add the UnitPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Units/UnitPrototype.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+
+namespace RuneForge.Content.Pipeline.Game.Units
+{
+    [XmlRoot]
+    public class UnitPrototype
+    {
+        [XmlElement(ElementName = "Name")]
+        public string Name { get; set; }
+    }
+}
