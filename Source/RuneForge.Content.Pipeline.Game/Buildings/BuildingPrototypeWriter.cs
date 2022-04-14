4f307b86ba3020de34329ad0653372f73f4aab37
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-14 16:47:51 +0300

    Add the BuildingPrototypeWriter content type writer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Buildings/BuildingPrototypeWriter.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline;
+using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+
+namespace RuneForge.Content.Pipeline.Game.Buildings
+{
+    [ContentTypeWriter]
+    public class BuildingPrototypeWriter : ContentTypeWriter<BuildingPrototype>
+    {
+        private const string c_runtimeReaderTypeName = "RuneForge.Game.Buildings.BuildingPrototypeReader, RuneForge.Game";
+
+        public override string GetRuntimeReader(TargetPlatform targetPlatform)
+        {
+            return c_runtimeReaderTypeName;
+        }
+
+        protected override void Write(ContentWriter writer, BuildingPrototype buildingPrototype)
+        {
+            writer.Write(buildingPrototype.Name);
+        }
+    }
+}
