ee9814731efeecf9868bb52f57f242f4fc989481
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-14 12:36:18 +0300

    Add the PlayerColor data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Players/PlayerColor.cs
@@ -0,0 +1,19 @@
+﻿using System.Xml.Serialization;
+
+namespace RuneForge.Content.Pipeline.Game.Players
+{
+    public class PlayerColor
+    {
+        [XmlAttribute(AttributeName = "MainColor")]
+        public string MainColor { get; set; }
+
+        [XmlAttribute(AttributeName = "EntityColorShadeA")]
+        public string EntityColorShadeA { get; set; }
+        [XmlAttribute(AttributeName = "EntityColorShadeB")]
+        public string EntityColorShadeB { get; set; }
+        [XmlAttribute(AttributeName = "EntityColorShadeC")]
+        public string EntityColorShadeC { get; set; }
+        [XmlAttribute(AttributeName = "EntityColorShadeD")]
+        public string EntityColorShadeD { get; set; }
+    }
+}
