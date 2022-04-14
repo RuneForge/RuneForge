e9fb368b6ff8ee7488e3c3b1ce3df3caeb33df35
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-14 17:02:57 +0300

    Add the UnitPrototypeReader content type reader class

---
+++ Source/RuneForge.Game/Units/UnitPrototypeReader.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Content;
+
+namespace RuneForge.Game.Units
+{
+    public class UnitPrototypeReader : ContentTypeReader<UnitPrototype>
+    {
+        protected override UnitPrototype Read(ContentReader reader, UnitPrototype existingInstance)
+        {
+            string name = reader.ReadString();
+
+            return new UnitPrototype(name);
+        }
+    }
+}
