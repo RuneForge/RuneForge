4f0060a03a446d2f1506b632eaf354055bc71614
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-14 17:03:11 +0300

    Add the BuildingPrototypeReader content type reader class

---
+++ Source/RuneForge.Game/Buildings/BuildingPrototypeReader.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Content;
+
+namespace RuneForge.Game.Buildings
+{
+    public class BuildingPrototypeReader : ContentTypeReader<BuildingPrototype>
+    {
+        protected override BuildingPrototype Read(ContentReader reader, BuildingPrototype existingInstance)
+        {
+            string name = reader.ReadString();
+
+            return new BuildingPrototype(name);
+        }
+    }
+}
