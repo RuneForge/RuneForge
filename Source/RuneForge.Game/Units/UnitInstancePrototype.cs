c1de2cee66c10c18c32fe3f4463b9e8b14b917b4
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-14 15:07:03 +0300

    Add the UnitInstancePrototype class

---
+++ Source/RuneForge.Game/Units/UnitInstancePrototype.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RuneForge.Game.Units
+{
+    public class UnitInstancePrototype
+    {
+        public Guid OwnerId { get; }
+
+        public UnitPrototype EntityPrototype { get; }
+
+        public UnitInstancePrototype(Guid ownerId, UnitPrototype entityPrototype)
+        {
+            OwnerId = ownerId;
+            EntityPrototype = entityPrototype;
+        }
+    }
+}
