35de8e60ca7e6805e6c694044b3c66cefdf782a5
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-14 15:15:38 +0300

    Add the BuildingPrototype class

---
+++ Source/RuneForge.Game/Buildings/BuildingPrototype.cs
@@ -0,0 +1,12 @@
+﻿namespace RuneForge.Game.Buildings
+{
+    public class BuildingPrototype
+    {
+        public string Name { get; }
+
+        public BuildingPrototype(string name)
+        {
+            Name = name;
+        }
+    }
+}
