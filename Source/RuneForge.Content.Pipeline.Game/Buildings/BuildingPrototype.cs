7bfb8d5ad5c424584201cffcf826d3faf14fa60d
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-14 16:45:54 +0300

    Add the BuildingPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Buildings/BuildingPrototype.cs
@@ -0,0 +1,11 @@
+﻿using System.Xml.Serialization;
+
+namespace RuneForge.Content.Pipeline.Game.Buildings
+{
+    [XmlRoot]
+    public class BuildingPrototype
+    {
+        [XmlElement(ElementName = "Name")]
+        public string Name { get; set; }
+    }
+}
