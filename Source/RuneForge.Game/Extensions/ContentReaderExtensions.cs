00df36654712971eb461d9b414b73b4798497893
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-15 22:48:39 +0300

    Add the ContentReaderExtensions static class with extension methods

---
+++ Source/RuneForge.Game/Extensions/ContentReaderExtensions.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+using Microsoft.Xna.Framework.Content;
+
+using RuneForge.Game.Entities;
+using RuneForge.Game.Entities.Attributes;
+using RuneForge.Game.Entities.Interfaces;
+
+namespace RuneForge.Game.Extensions
+{
+    public static class ContentReaderExtensions
+    {
+        public static ComponentPrototype ReadComponentPrototype(this ContentReader contentReader)
+        {
+            string componentPrototypeTypeName = contentReader.ReadString();
+            Type componentPrototypeType = Type.GetType(componentPrototypeTypeName);
+            ComponentPrototypeReaderAttribute[] attributes = componentPrototypeType
+                .GetCustomAttributes(typeof(ComponentPrototypeReaderAttribute), false)
+                .Cast<ComponentPrototypeReaderAttribute>()
+                .ToArray();
+
+            if (attributes.Length == 0)
+                throw new InvalidOperationException($"Unable to find the {nameof(ComponentPrototypeReaderAttribute)} attribute on the {componentPrototypeType.Name} type.");
+            else
+            {
+                ComponentPrototypeReaderAttribute attribute = attributes.Single();
+
+                Type componentPrototypeReaderType = attribute.ComponentPrototypeReaderType;
+                IComponentPrototypeReader componentPrototypeReader = (IComponentPrototypeReader)Activator.CreateInstance(componentPrototypeReaderType);
+
+                return componentPrototypeReader.ReadComponentPrototype(contentReader);
+            }
+        }
+    }
+}
