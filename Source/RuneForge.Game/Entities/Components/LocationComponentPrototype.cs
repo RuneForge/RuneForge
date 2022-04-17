344dac1555e76ad2f8e856ad500bf1ea0d292409
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-17 19:17:28 +0300

    Add the LocationComponentPrototype class

---
+++ Source/RuneForge.Game/Entities/Components/LocationComponentPrototype.cs
@@ -0,0 +1,19 @@
+﻿namespace RuneForge.Game.Entities.Components
+{
+    public class LocationComponentPrototype : ComponentPrototype
+    {
+        public int XCells { get; }
+        public int YCells { get; }
+
+        public int WidthCells { get; }
+        public int HeightCells { get; }
+
+        public LocationComponentPrototype(int xCells, int yCells, int widthCells, int heightCells)
+        {
+            XCells = xCells;
+            YCells = yCells;
+            WidthCells = widthCells;
+            HeightCells = heightCells;
+        }
+    }
+}
