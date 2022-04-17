7213e32eed42983bfb9890697b9d505a220b5788
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-17 19:52:21 +0300

    Add the LocationComponentPrototypeWriter class

---
+++ Source/RuneForge.Content.Pipeline.Game/Entities/ComponentWriters/LocationComponentPrototypeWriter.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+
+using RuneForge.Content.Pipeline.Game.Entities.Components;
+
+namespace RuneForge.Content.Pipeline.Game.Entities.ComponentWriters
+{
+    public class LocationComponentPrototypeWriter : ComponentPrototypeWriter<LocationComponentPrototype>
+    {
+        public override void WriteComponentPrototype(ContentWriter contentWriter, LocationComponentPrototype componentPrototype)
+        {
+            contentWriter.Write(componentPrototype.X);
+            contentWriter.Write(componentPrototype.Y);
+            contentWriter.Write(componentPrototype.Width);
+            contentWriter.Write(componentPrototype.Height);
+        }
+    }
+}
