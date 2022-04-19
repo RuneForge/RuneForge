f70d8ca8f8293277ca266246e096ff83c0ceba7e
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-19 16:05:23 +0300

    Make properties of the LocationComponentPrototype class nullable

--- Source/RuneForge.Game/Entities/Components/LocationComponentPrototype.cs
+++ Source/RuneForge.Game/Entities/Components/LocationComponentPrototype.cs
@@ -8,13 +8,13 @@ namespace RuneForge.Game.Entities.Components
     [ComponentPrototypeReader(typeof(LocationComponentPrototypeReader))]
     public class LocationComponentPrototype : ComponentPrototype
     {
-        public int XCells { get; }
-        public int YCells { get; }
+        public int? XCells { get; }
+        public int? YCells { get; }
 
-        public int WidthCells { get; }
-        public int HeightCells { get; }
+        public int? WidthCells { get; }
+        public int? HeightCells { get; }
 
-        public LocationComponentPrototype(int xCells, int yCells, int widthCells, int heightCells)
+        public LocationComponentPrototype(int? xCells, int? yCells, int? widthCells, int? heightCells)
         {
             XCells = xCells;
             YCells = yCells;
