f1262ad74d66e2c2eb9bfca48867e14b259a0932
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-19 23:34:19 +0300

    Add the TextureAtlasComponentPrototypeReader class

---
+++ Source/RuneForge.Game/Entities/ComponentReaders/TextureAtlasComponentPrototypeReader.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Content;
+
+using RuneForge.Game.Entities.Components;
+
+namespace RuneForge.Game.Entities.ComponentReaders
+{
+    public class TextureAtlasComponentPrototypeReader : ComponentPrototypeReader<TextureAtlasComponentPrototype>
+    {
+        public override TextureAtlasComponentPrototype ReadTypedComponentPrototype(ContentReader contentReader)
+        {
+            string textureAtlasAssetName = contentReader.ReadString();
+            bool hasPlayerColor = contentReader.ReadBoolean();
+            return new TextureAtlasComponentPrototype(textureAtlasAssetName, hasPlayerColor);
+        }
+    }
+}
--- Source/RuneForge.Game/Entities/Components/TextureAtlasComponentPrototype.cs
+++ Source/RuneForge.Game/Entities/Components/TextureAtlasComponentPrototype.cs
@@ -1,5 +1,9 @@
-﻿namespace RuneForge.Game.Entities.Components
+﻿using RuneForge.Game.Entities.Attributes;
+using RuneForge.Game.Entities.ComponentReaders;
+
+namespace RuneForge.Game.Entities.Components
 {
+    [ComponentPrototypeReader(typeof(TextureAtlasComponentPrototypeReader))]
     public class TextureAtlasComponentPrototype : ComponentPrototype
     {
         public string TextureAtlasAssetName { get; }
