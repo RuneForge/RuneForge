217ae6f108b70a7b7b84af38ddcc779e7ef1dfbe
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-20 00:24:38 +0300

    Add the AnimationAtlasComponentPrototype class

---
+++ Source/RuneForge.Game/Entities/Components/AnimationAtlasComponentPrototype.cs
@@ -0,0 +1,15 @@
+﻿namespace RuneForge.Game.Entities.Components
+{
+    public class AnimationAtlasComponentPrototype : ComponentPrototype
+    {
+        public string AnimationAtlasAssetName { get; }
+
+        public bool HasPlayerColor { get; }
+
+        public AnimationAtlasComponentPrototype(string animationAtlasAssetName, bool hasPlayerColor)
+        {
+            AnimationAtlasAssetName = animationAtlasAssetName;
+            HasPlayerColor = hasPlayerColor;
+        }
+    }
+}
