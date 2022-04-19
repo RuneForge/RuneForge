ac541dfa75967bd988dc4174b72b0afe186b26be
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-19 23:29:54 +0300

    Add the TextureAtlasComponent class

---
+++ Source/RuneForge.Game/Entities/Components/TextureAtlasComponent.cs
@@ -0,0 +1,15 @@
+﻿namespace RuneForge.Game.Entities.Components
+{
+    public class TextureAtlasComponent : Component
+    {
+        public string TextureAtlasAssetName { get; }
+
+        public bool HasPlayerColor { get; }
+
+        public TextureAtlasComponent(string textureAtlasAssetName, bool hasPlayerColor)
+        {
+            TextureAtlasAssetName = textureAtlasAssetName;
+            HasPlayerColor = hasPlayerColor;
+        }
+    }
+}
