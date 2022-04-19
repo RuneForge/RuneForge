3346d62907b25077b36f0b70400d66182e22e252
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-19 18:03:22 +0300

    Add the DirectionComponentPrototype class

---
+++ Source/RuneForge.Game/Entities/Components/DirectionComponentPrototype.cs
@@ -0,0 +1,14 @@
+﻿using RuneForge.Game.Maps;
+
+namespace RuneForge.Game.Entities.Components
+{
+    public class DirectionComponentPrototype : ComponentPrototype
+    {
+        public Directions? Direction { get; }
+
+        public DirectionComponentPrototype(Directions? direction)
+        {
+            Direction = direction;
+        }
+    }
+}
