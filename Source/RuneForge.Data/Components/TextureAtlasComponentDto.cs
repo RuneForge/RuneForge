d60c6195bae4134f714d1dd55ff111aeb8388b45
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-19 23:42:28 +0300

    Add the TextureAtlasComponentDto data transfer class

---
+++ Source/RuneForge.Data/Components/TextureAtlasComponentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RuneForge.Data.Components
+{
+    public class TextureAtlasComponentDto : ComponentDto
+    {
+        public string TextureAtlasAssetName { get; set; }
+
+        public bool HasPlayerColor { get; set; }
+    }
+}
