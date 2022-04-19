d5f116c97937645053d721e00f4fd1b7cd690394
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-19 23:30:00 +0300

    Add the TextureAtlasComponentPrototype class

---
+++ Source/RuneForge.Game/Entities/Components/TextureAtlasComponentPrototype.cs
@@ -0,0 +1,15 @@
+﻿namespace RuneForge.Game.Entities.Components
+{
+    public class TextureAtlasComponentPrototype : ComponentPrototype
+    {
+        public string TextureAtlasAssetName { get; }
+
+        public bool HasPlayerColor { get; }
+
+        public TextureAtlasComponentPrototype(string textureAtlasAssetName, bool hasPlayerColor)
+        {
+            TextureAtlasAssetName = textureAtlasAssetName;
+            HasPlayerColor = hasPlayerColor;
+        }
+    }
+}
