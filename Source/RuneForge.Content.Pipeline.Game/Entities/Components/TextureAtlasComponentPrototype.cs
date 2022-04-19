d41adb842e093e2c24877299a239cfea957af942
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-19 23:51:50 +0300

    Add the TextureAtlasComponentPrototypeWriter class

--- Source/RuneForge.Content.Pipeline.Game/Entities/Components/TextureAtlasComponentPrototype.cs
+++ Source/RuneForge.Content.Pipeline.Game/Entities/Components/TextureAtlasComponentPrototype.cs
@@ -1,7 +1,11 @@
 ﻿using System.Xml.Serialization;
 
+using RuneForge.Content.Pipeline.Game.Entities.Attributes;
+using RuneForge.Content.Pipeline.Game.Entities.ComponentWriters;
+
 namespace RuneForge.Content.Pipeline.Game.Entities.Components
 {
+    [ComponentPrototypeWriter(typeof(TextureAtlasComponentPrototypeWriter))]
     public class TextureAtlasComponentPrototype : ComponentPrototype
     {
         private const string c_runtimeTypeName = "RuneForge.Game.Entities.Components.TextureAtlasComponentPrototype, RuneForge.Game";
