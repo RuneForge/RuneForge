6300ae56a9b425f1ed7ce825f25753833d582e3e
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-19 23:48:50 +0300

    Add the TextureAtlasComponentPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Entities/Components/TextureAtlasComponentPrototype.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace RuneForge.Content.Pipeline.Game.Entities.Components
+{
+    public class TextureAtlasComponentPrototype : ComponentPrototype
+    {
+        private const string c_runtimeTypeName = "RuneForge.Game.Entities.Components.TextureAtlasComponentPrototype, RuneForge.Game";
+
+        [XmlAttribute(AttributeName = "TextureAtlasAssetName")]
+        public string TextureAtlasAssetName { get; set; }
+
+        [XmlAttribute(AttributeName = "HasPlayerColor")]
+        public bool HasPlayerColor { get; set; }
+
+        public override string GetRuntimeTypeName()
+        {
+            return c_runtimeTypeName;
+        }
+    }
+}
