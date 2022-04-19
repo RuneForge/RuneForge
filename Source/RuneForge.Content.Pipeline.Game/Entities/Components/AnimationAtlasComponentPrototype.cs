27bf8052ba21aa7647b37621f9380fd5828e5de2
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-20 00:36:56 +0300

    Add the AnimationAtlasComponentPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Entities/Components/AnimationAtlasComponentPrototype.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace RuneForge.Content.Pipeline.Game.Entities.Components
+{
+    public class AnimationAtlasComponentPrototype : ComponentPrototype
+    {
+        private const string c_runtimeTypeName = "RuneForge.Game.Entities.Components.AnimationAtlasComponentPrototype, RuneForge.Game";
+
+        [XmlAttribute(AttributeName = "AnimationAtlasAssetName")]
+        public string AnimationAtlasAssetName { get; set; }
+
+        [XmlAttribute(AttributeName = "HasPlayerColor")]
+        public bool HasPlayerColor { get; set; }
+
+        public override string GetRuntimeTypeName()
+        {
+            return c_runtimeTypeName;
+        }
+    }
+}
