d77d373dfd995abd4f233b2c8f0f5f83e49cffa8
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-20 19:06:43 +0300

    Add the MovementComponentPrototype class

---
+++ Source/RuneForge.Game/Components/Implementations/MovementComponentPrototype.cs
@@ -0,0 +1,12 @@
+﻿namespace RuneForge.Game.Components.Implementations
+{
+    public class MovementComponentPrototype : ComponentPrototype
+    {
+        public float MovementSpeed { get; }
+
+        public MovementComponentPrototype(float movementSpeed)
+        {
+            MovementSpeed = movementSpeed;
+        }
+    }
+}
