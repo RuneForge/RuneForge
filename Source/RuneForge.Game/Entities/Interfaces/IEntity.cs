7eb4bf4fea6f399737dd2b24c2723fb534f60d44
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-20 13:51:02 +0300

    Add parameter type restrictions to methods in the interface of the Entity class

--- Source/RuneForge.Game/Entities/Interfaces/IEntity.cs
+++ Source/RuneForge.Game/Entities/Interfaces/IEntity.cs
@@ -8,12 +8,12 @@ namespace RuneForge.Game.Entities.Interfaces
         public ReadOnlyCollection<IComponent> Components { get; }
 
         public IComponent GetComponentOfType(Type componentType);
-        public TComponent GetComponentOfType<TComponent>() where TComponent : IComponent;
+        public TComponent GetComponentOfType<TComponent>() where TComponent : class, IComponent;
 
         public bool TryGetComponentOfType(Type componentType, out IComponent component);
-        public bool TryGetComponentOfType<TComponent>(out IComponent component) where TComponent : IComponent;
+        public bool TryGetComponentOfType<TComponent>(out TComponent component) where TComponent : class, IComponent;
 
         public bool HasComponentOfType(Type componentType);
-        public bool HasComponentOfType<TComponent>() where TComponent : IComponent;
+        public bool HasComponentOfType<TComponent>() where TComponent : class, IComponent;
     }
 }
