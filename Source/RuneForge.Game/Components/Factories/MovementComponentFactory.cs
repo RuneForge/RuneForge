c1fa94ba94e3bbaf0f37459d2f390a631aa80c37
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-20 19:11:36 +0300

    Add the MovementComponentFactory class

---
+++ Source/RuneForge.Game/Components/Factories/MovementComponentFactory.cs
@@ -0,0 +1,12 @@
+﻿using RuneForge.Game.Components.Implementations;
+
+namespace RuneForge.Game.Components.Factories
+{
+    public class MovementComponentFactory : ComponentFactory<MovementComponent, MovementComponentPrototype>
+    {
+        public override MovementComponent CreateComponentFromPrototype(MovementComponentPrototype componentPrototype, MovementComponentPrototype componentPrototypeOverride)
+        {
+            return new MovementComponent(componentPrototype.MovementSpeed);
+        }
+    }
+}
