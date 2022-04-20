c2aa4095821e114174f2d75532fc3c6e46809f6f
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-20 19:08:52 +0300

    Add the MovementComponentPrototypeReader class

--- Source/RuneForge.Game/Components/Implementations/MovementComponentPrototype.cs
+++ Source/RuneForge.Game/Components/Implementations/MovementComponentPrototype.cs
@@ -1,5 +1,9 @@
-﻿namespace RuneForge.Game.Components.Implementations
+﻿using RuneForge.Game.Components.Attributes;
+using RuneForge.Game.Components.PrototypeReaders;
+
+namespace RuneForge.Game.Components.Implementations
 {
+    [ComponentPrototypeReader(typeof(MovementComponentPrototypeReader))]
     public class MovementComponentPrototype : ComponentPrototype
     {
         public float MovementSpeed { get; }
---
+++ Source/RuneForge.Game/Components/PrototypeReaders/MovementComponentPrototypeReader.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Content;
+
+using RuneForge.Game.Components.Implementations;
+
+namespace RuneForge.Game.Components.PrototypeReaders
+{
+    public class MovementComponentPrototypeReader : ComponentPrototypeReader<MovementComponentPrototype>
+    {
+        public override MovementComponentPrototype ReadTypedComponentPrototype(ContentReader contentReader)
+        {
+            float movementSpeed = contentReader.ReadSingle();
+            return new MovementComponentPrototype(movementSpeed);
+        }
+    }
+}
