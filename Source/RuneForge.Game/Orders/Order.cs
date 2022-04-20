09328e0eb07bb560ff68c1bf390f6c679eaf6f69
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-20 21:10:33 +0300

    Add the Order abstract class

---
+++ Source/RuneForge.Game/Orders/Order.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+
+using RuneForge.Game.Entities;
+
+namespace RuneForge.Game.Orders
+{
+    public abstract class Order
+    {
+        public Entity Entity { get; }
+
+        public OrderState State { get; protected set; }
+
+        protected Order(Entity entity)
+        {
+            Entity = entity;
+            State = OrderState.Scheduled;
+        }
+
+        public virtual void Run()
+        {
+            State = OrderState.InProgress;
+        }
+
+        public virtual void Complete()
+        {
+            State = OrderState.Completed;
+        }
+
+        public virtual void Cancel()
+        {
+            State = OrderState.Cancelled;
+        }
+
+        public virtual void Update(GameTime gameTime, out bool stateChanged)
+        {
+            stateChanged = false;
+        }
+    }
+}
