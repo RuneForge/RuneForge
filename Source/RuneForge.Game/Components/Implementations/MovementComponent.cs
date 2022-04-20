98b94146cc9b712884bf45933178cdcdff6966df
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-20 19:05:12 +0300

    Add the MovementComponent class

---
+++ Source/RuneForge.Game/Components/Implementations/MovementComponent.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace RuneForge.Game.Components.Implementations
+{
+    public class MovementComponent : Component
+    {
+        public float MovementSpeed { get; }
+
+        public int OriginCellX { get; set; }
+        public int OriginCellY { get; set; }
+
+        public int DestinationCellX { get; set; }
+        public int DestinationCellY { get; set; }
+
+        public bool MovementScheduled { get; set; }
+        public bool MovementInProgress { get; set; }
+        public bool PathBlocked { get; set; }
+
+        public MovementComponent(float movementSpeed)
+        {
+            MovementSpeed = movementSpeed;
+            OriginCellX = -1;
+            OriginCellY = -1;
+            DestinationCellX = -1;
+            DestinationCellY = -1;
+            MovementScheduled = false;
+            MovementInProgress = false;
+            PathBlocked = false;
+        }
+
+        public Point OriginCell
+        {
+            get => new Point(OriginCellX, OriginCellY);
+            set => (OriginCellX, OriginCellY) = value;
+        }
+
+        public Point DestinationCell
+        {
+            get => new Point(DestinationCellX, DestinationCellY);
+            set => (DestinationCellX, DestinationCellY) = value;
+        }
+    }
+}
