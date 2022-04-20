9932956c8642186d77270bceea2a7dd2ee429d5d
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-20 19:14:30 +0300

    Add the MovementComponentPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/MovementComponentPrototype.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace RuneForge.Content.Pipeline.Game.Components.Implementations
+{
+    public class MovementComponentPrototype : ComponentPrototype
+    {
+        private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.MovementComponentPrototype, RuneForge.Game";
+
+        [XmlAttribute(AttributeName = "MovementSpeed")]
+        public float MovementSpeed { get; set; }
+
+        public override string GetRuntimeTypeName()
+        {
+            return c_runtimeTypeName;
+        }
+    }
+}
