c1a3a6691ba655bc9bc36ca6ba549eb7abb173db
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-20 19:15:59 +0300

    Add the MovementComponentPrototypeWriter class

--- Source/RuneForge.Content.Pipeline.Game/Components/Implementations/MovementComponentPrototype.cs
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/MovementComponentPrototype.cs
@@ -1,7 +1,11 @@
 ﻿using System.Xml.Serialization;
 
+using RuneForge.Content.Pipeline.Game.Components.Attributes;
+using RuneForge.Content.Pipeline.Game.Components.PrototypeWriters;
+
 namespace RuneForge.Content.Pipeline.Game.Components.Implementations
 {
+    [ComponentPrototypeWriter(typeof(MovementComponentPrototypeWriter))]
     public class MovementComponentPrototype : ComponentPrototype
     {
         private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.MovementComponentPrototype, RuneForge.Game";
---
+++ Source/RuneForge.Content.Pipeline.Game/Components/PrototypeWriters/MovementComponentPrototypeWriter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+
+using RuneForge.Content.Pipeline.Game.Components.Implementations;
+
+namespace RuneForge.Content.Pipeline.Game.Components.PrototypeWriters
+{
+    public class MovementComponentPrototypeWriter : ComponentPrototypeWriter<MovementComponentPrototype>
+    {
+        public override void WriteComponentPrototype(ContentWriter contentWriter, MovementComponentPrototype componentPrototype)
+        {
+            contentWriter.Write(componentPrototype.MovementSpeed);
+        }
+    }
+}
