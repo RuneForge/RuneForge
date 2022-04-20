b319b18221eca36d58fbf5c16db55232d69de63c
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-20 13:51:26 +0300

    Add the SpriteBatchExtensions static class with extension methods

---
+++ Source/RuneForge.Core/Rendering/Extensions/SpriteBatchExtensions.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+using RuneForge.Core.TextureAtlases;
+
+namespace RuneForge.Core.Rendering.Extensions
+{
+    public static class SpriteBatchExtensions
+    {
+        public static void DrawTextureRegionUsingExternalTexture(
+            this SpriteBatch spriteBatch,
+            TextureRegion2D textureRegion,
+            Texture2D externalTexture,
+            Rectangle destinationRectangle,
+            Color color
+            )
+        {
+            spriteBatch.Draw(externalTexture, destinationRectangle, textureRegion.Bounds, color);
+        }
+    }
+}
