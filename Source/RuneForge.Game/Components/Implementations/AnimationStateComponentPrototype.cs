8e4b97f07d73e8cdc66d74d72684c175a363c1cb
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-21 20:55:03 +0300

    Add the AnimationStateComponentFactory class

--- Source/RuneForge.Game/Components/Implementations/AnimationStateComponentPrototype.cs
+++ Source/RuneForge.Game/Components/Implementations/AnimationStateComponentPrototype.cs
@@ -1,8 +1,10 @@
 ﻿using RuneForge.Game.Components.Attributes;
+using RuneForge.Game.Components.Factories;
 using RuneForge.Game.Components.PrototypeReaders;
 
 namespace RuneForge.Game.Components.Implementations
 {
+    [ComponentFactory(typeof(AnimationStateComponentFactory))]
     [ComponentPrototypeReader(typeof(AnimationStateComponentPrototypeReader))]
     public class AnimationStateComponentPrototype : ComponentPrototype
     {
