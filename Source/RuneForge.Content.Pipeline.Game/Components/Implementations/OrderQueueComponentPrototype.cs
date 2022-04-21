fbef2a8edea0ea762f2c1e70595c09746b7c117c
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-21 16:30:04 +0300

    Add the OrderQueueComponentPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/OrderQueueComponentPrototype.cs
@@ -0,0 +1,12 @@
+﻿namespace RuneForge.Content.Pipeline.Game.Components.Implementations
+{
+    public class OrderQueueComponentPrototype : ComponentPrototype
+    {
+        private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.OrderQueueComponentPrototype, RuneForge.Game";
+
+        public override string GetRuntimeTypeName()
+        {
+            return c_runtimeTypeName;
+        }
+    }
+}
