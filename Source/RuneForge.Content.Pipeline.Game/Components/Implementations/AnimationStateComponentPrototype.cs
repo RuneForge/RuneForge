89de862b318377838505b3bf3ea80a01ce9c3aca
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-21 21:12:48 +0300

    Add the AnimationStateComponentPrototypeWriter class

--- Source/RuneForge.Content.Pipeline.Game/Components/Implementations/AnimationStateComponentPrototype.cs
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/AnimationStateComponentPrototype.cs
@@ -1,5 +1,9 @@
-﻿namespace RuneForge.Content.Pipeline.Game.Components.Implementations
+﻿using RuneForge.Content.Pipeline.Game.Components.Attributes;
+using RuneForge.Content.Pipeline.Game.Components.PrototypeWriters;
+
+namespace RuneForge.Content.Pipeline.Game.Components.Implementations
 {
+    [ComponentPrototypeWriter(typeof(AnimationStateComponentPrototypeWriter))]
     public class AnimationStateComponentPrototype : ComponentPrototype
     {
         private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.AnimationStateComponentPrototype, RuneForge.Game";
