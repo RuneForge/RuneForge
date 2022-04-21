a94aea060b004dcbe6961ccdd2580539e7be846d
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-21 16:20:26 +0300

    Add the OrderQueueComponentDto data transfer class

---
+++ Source/RuneForge.Data/Components/OrderQueueComponentDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+using RuneForge.Data.Orders;
+
+namespace RuneForge.Data.Components
+{
+    public class OrderQueueComponentDto : ComponentDto
+    {
+        public OrderDto CurrentOrder { get; set; }
+
+        public List<OrderDto> PendingOrders { get; set; }
+    }
+}
