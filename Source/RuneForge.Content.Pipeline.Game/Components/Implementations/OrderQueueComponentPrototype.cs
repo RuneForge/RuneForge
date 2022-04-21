92a5bcbcdfc86f6fd8de073604d50d4303cb6a93
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-21 16:31:11 +0300

    Add the OrderQueueComponentPrototypeWriter class

--- Source/RuneForge.Content.Pipeline.Game/Components/Implementations/OrderQueueComponentPrototype.cs
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/OrderQueueComponentPrototype.cs
@@ -1,5 +1,9 @@
-﻿namespace RuneForge.Content.Pipeline.Game.Components.Implementations
+﻿using RuneForge.Content.Pipeline.Game.Components.Attributes;
+using RuneForge.Content.Pipeline.Game.Components.PrototypeWriters;
+
+namespace RuneForge.Content.Pipeline.Game.Components.Implementations
 {
+    [ComponentPrototypeWriter(typeof(OrderQueueComponentPrototypeWriter))]
     public class OrderQueueComponentPrototype : ComponentPrototype
     {
         private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.OrderQueueComponentPrototype, RuneForge.Game";
---
+++ Source/RuneForge.Content.Pipeline.Game/Components/PrototypeWriters/OrderQueueComponentPrototypeWriter.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+
+using RuneForge.Content.Pipeline.Game.Components.Implementations;
+
+namespace RuneForge.Content.Pipeline.Game.Components.PrototypeWriters
+{
+    public class OrderQueueComponentPrototypeWriter : ComponentPrototypeWriter<OrderQueueComponentPrototype>
+    {
+        public override void WriteComponentPrototype(ContentWriter contentWriter, OrderQueueComponentPrototype componentPrototype)
+        {
+        }
+    }
+}
