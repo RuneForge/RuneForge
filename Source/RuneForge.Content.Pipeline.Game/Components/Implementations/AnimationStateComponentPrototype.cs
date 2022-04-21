72c4828d8bd1dbbd25537237609d5f7fdb635381
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-21 21:04:53 +0300

    Add the AnimationStateComponentPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/AnimationStateComponentPrototype.cs
@@ -0,0 +1,12 @@
+﻿namespace RuneForge.Content.Pipeline.Game.Components.Implementations
+{
+    public class AnimationStateComponentPrototype : ComponentPrototype
+    {
+        private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.AnimationStateComponentPrototype, RuneForge.Game";
+
+        public override string GetRuntimeTypeName()
+        {
+            return c_runtimeTypeName;
+        }
+    }
+}
