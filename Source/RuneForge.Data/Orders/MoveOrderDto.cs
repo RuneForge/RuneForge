14077c0d6a01d9eba2fd0e0f2312b88ce66ed277
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-21 18:29:07 +0300

    Add the MoveOrderDto data transfer class

---
+++ Source/RuneForge.Data/Orders/MoveOrderDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RuneForge.Data.Orders
+{
+    public class MoveOrderDto : OrderDto
+    {
+        public int DestinationX { get; set; }
+        public int DestinationY { get; set; }
+
+        public bool CancellationRequested { get; set; }
+    }
+}
