5399c703914cb59e0b6661136477f31f15094528
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-21 20:57:45 +0300

    Add the AnimationStateComponentDto data transfer class

---
+++ Source/RuneForge.Data/Components/AnimationStateComponentDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RuneForge.Data.Components
+{
+    public class AnimationStateComponentDto : ComponentDto
+    {
+        public string AnimationName { get; set; }
+
+        public TimeSpan ElapsedTime { get; set; }
+
+        public bool ResetRequested { get; set; }
+    }
+}
