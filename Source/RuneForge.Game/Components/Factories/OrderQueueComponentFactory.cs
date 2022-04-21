5fbbdcc5a00f1d3a2bccc72d8da850b83d2c2fb1
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-21 15:49:57 +0300

    Add the OrderQueueComponentFactory class

---
+++ Source/RuneForge.Game/Components/Factories/OrderQueueComponentFactory.cs
@@ -0,0 +1,12 @@
+﻿using RuneForge.Game.Components.Implementations;
+
+namespace RuneForge.Game.Components.Factories
+{
+    public class OrderQueueComponentFactory : ComponentFactory<OrderQueueComponent, OrderQueueComponentPrototype>
+    {
+        public override OrderQueueComponent CreateComponentFromPrototype(OrderQueueComponentPrototype componentPrototype, OrderQueueComponentPrototype componentPrototypeOverride)
+        {
+            return new OrderQueueComponent();
+        }
+    }
+}
--- Source/RuneForge.Game/Components/Implementations/OrderQueueComponentPrototype.cs
+++ Source/RuneForge.Game/Components/Implementations/OrderQueueComponentPrototype.cs
@@ -1,8 +1,10 @@
 ﻿using RuneForge.Game.Components.Attributes;
+using RuneForge.Game.Components.Factories;
 using RuneForge.Game.Components.PrototypeReaders;
 
 namespace RuneForge.Game.Components.Implementations
 {
+    [ComponentFactory(typeof(OrderQueueComponentFactory))]
     [ComponentPrototypeReader(typeof(OrderQueueComponentPrototypeReader))]
     public class OrderQueueComponentPrototype : ComponentPrototype
     {
