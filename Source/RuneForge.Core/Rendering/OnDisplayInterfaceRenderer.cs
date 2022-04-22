043c2e8862c3b20ebf61bb1dc3c8645d9ff4a689
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-22 21:09:08 +0300

    Add the OnDisplayInterfaceRenderer class

---
+++ Source/RuneForge.Core/Rendering/OnDisplayInterfaceRenderer.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace RuneForge.Core.Rendering
+{
+    public class OnDisplayInterfaceRenderer : Renderer
+    {
+        public override void Draw(GameTime gameTime)
+        {
+        }
+
+        public override void LoadContent()
+        {
+        }
+    }
+}
