0b22cebb85b2c3714c744646c9b18fb205d96452
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-22 19:04:02 +0300

    Add the EntitySelectionContext class

---
+++ Source/RuneForge.Core/Controllers/Interfaces/IEntitySelectionContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using RuneForge.Game.Entities;
+
+namespace RuneForge.Core.Controllers.Interfaces
+{
+    public interface IEntitySelectionContext
+    {
+        public Entity Entity { get; set; }
+
+        public event EventHandler<EventArgs> EntitySelected;
+        public event EventHandler<EventArgs> EntitySelectionDropped;
+    }
+}
