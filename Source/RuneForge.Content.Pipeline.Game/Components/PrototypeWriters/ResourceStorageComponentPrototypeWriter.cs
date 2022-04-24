85602efce2d76eec1b3073a4ddbfc158db7765fd
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 01:37:44 +0300

    Add the ResourceStorageComponentPrototypeWriter class

---
+++ Source/RuneForge.Content.Pipeline.Game/Components/PrototypeWriters/ResourceStorageComponentPrototypeWriter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+
+using RuneForge.Content.Pipeline.Game.Components.Implementations;
+
+namespace RuneForge.Content.Pipeline.Game.Components.PrototypeWriters
+{
+    public class ResourceStorageComponentPrototypeWriter : ComponentPrototypeWriter<ResourceStorageComponentPrototype>
+    {
+        public override void WriteComponentPrototype(ContentWriter contentWriter, ResourceStorageComponentPrototype componentPrototype)
+        {
+            contentWriter.Write((int)componentPrototype.AcceptedResourceTypes);
+            contentWriter.Write(componentPrototype.TransferTimeMilliseconds);
+        }
+    }
+}
