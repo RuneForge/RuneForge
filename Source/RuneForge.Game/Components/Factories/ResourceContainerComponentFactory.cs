9b559cdf98dfad7328d28b0e8e760deed409ea7b
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-24 23:55:57 +0300

    Add the ResourceContainerComponentFactory class

---
+++ Source/RuneForge.Game/Components/Factories/ResourceContainerComponentFactory.cs
@@ -0,0 +1,15 @@
+﻿using RuneForge.Game.Components.Implementations;
+
+namespace RuneForge.Game.Components.Factories
+{
+    public class ResourceContainerComponentFactory : ComponentFactory<ResourceContainerComponent, ResourceContainerComponentPrototype>
+    {
+        public override ResourceContainerComponent CreateComponentFromPrototype(ResourceContainerComponentPrototype componentPrototype, ResourceContainerComponentPrototype componentPrototypeOverride)
+        {
+            return new ResourceContainerComponent()
+            {
+                GoldAmount = componentPrototype.GoldAmount,
+            };
+        }
+    }
+}
--- Source/RuneForge.Game/Components/Implementations/ResourceContainerComponentPrototype.cs
+++ Source/RuneForge.Game/Components/Implementations/ResourceContainerComponentPrototype.cs
@@ -1,8 +1,10 @@
 ﻿using RuneForge.Game.Components.Attributes;
+using RuneForge.Game.Components.Factories;
 using RuneForge.Game.Components.PrototypeReaders;
 
 namespace RuneForge.Game.Components.Implementations
 {
+    [ComponentFactory(typeof(ResourceContainerComponentFactory))]
     [ComponentPrototypeReader(typeof(ResourceContainerComponentPrototypeReader))]
     public class ResourceContainerComponentPrototype : ComponentPrototype
     {
