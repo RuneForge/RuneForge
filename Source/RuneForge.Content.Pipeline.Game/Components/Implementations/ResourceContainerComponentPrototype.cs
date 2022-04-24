32603a4e4d2aefcec29f255aca182d429927062b
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 00:10:38 +0300

    Add the ResourceContainerComponentPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/ResourceContainerComponentPrototype.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace RuneForge.Content.Pipeline.Game.Components.Implementations
+{
+    public class ResourceContainerComponentPrototype : ComponentPrototype
+    {
+        private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.ResourceContainerComponentPrototype, RuneForge.Game";
+
+        [XmlAttribute(AttributeName = "GoldAmount")]
+        public decimal GoldAmount { get; set; }
+
+        public override string GetRuntimeTypeName()
+        {
+            return c_runtimeTypeName;
+        }
+    }
+}
