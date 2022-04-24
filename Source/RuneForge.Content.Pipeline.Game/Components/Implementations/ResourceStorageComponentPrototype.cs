95924f11380c2c57ee43ee5ad0f21a28ec5e5bce
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 01:35:43 +0300

    Add the ResourceStorageComponentPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/ResourceStorageComponentPrototype.cs
@@ -0,0 +1,22 @@
+﻿using System.Xml.Serialization;
+
+using RuneForge.Game.Components.Entities;
+
+namespace RuneForge.Content.Pipeline.Game.Components.Implementations
+{
+    public class ResourceStorageComponentPrototype : ComponentPrototype
+    {
+        private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.ResourceStorageComponentPrototype, RuneForge.Game";
+
+        [XmlAttribute(AttributeName = "AcceptedResourceTypes")]
+        public ResourceTypes AcceptedResourceTypes { get; set; }
+
+        [XmlAttribute(AttributeName = "TransferTimeMilliseconds")]
+        public float TransferTimeMilliseconds { get; set; }
+
+        public override string GetRuntimeTypeName()
+        {
+            return c_runtimeTypeName;
+        }
+    }
+}
