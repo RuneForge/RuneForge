c183f1f2fa440b8ba59aab304408deb7b57dc9af
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-24 23:51:48 +0300

    Add the ResourceContainerComponentPrototypeReader class

---
+++ Source/RuneForge.Game/Components/PrototypeReaders/ResourceContainerComponentPrototypeReader.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Content;
+
+using RuneForge.Game.Components.Implementations;
+
+namespace RuneForge.Game.Components.PrototypeReaders
+{
+    public class ResourceContainerComponentPrototypeReader : ComponentPrototypeReader<ResourceContainerComponentPrototype>
+    {
+        public override ResourceContainerComponentPrototype ReadTypedComponentPrototype(ContentReader contentReader)
+        {
+            decimal goldAmount = contentReader.ReadDecimal();
+            return new ResourceContainerComponentPrototype(goldAmount);
+        }
+    }
+}
