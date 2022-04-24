539252ede9efd9b4ab65fece81289ab4c395dea8
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 01:30:30 +0300

    Add the ResourceStorageComponentFactory class

--- Source/RuneForge.Game/Components/Implementations/ResourceStorageComponentPrototype.cs
+++ Source/RuneForge.Game/Components/Implementations/ResourceStorageComponentPrototype.cs
@@ -2,10 +2,12 @@
 
 using RuneForge.Game.Components.Attributes;
 using RuneForge.Game.Components.Entities;
+using RuneForge.Game.Components.Factories;
 using RuneForge.Game.Components.PrototypeReaders;
 
 namespace RuneForge.Game.Components.Implementations
 {
+    [ComponentFactory(typeof(ResourceStorageComponentFactory))]
     [ComponentPrototypeReader(typeof(ResourceStorageComponentPrototypeReader))]
     public class ResourceStorageComponentPrototype : ComponentPrototype
     {
