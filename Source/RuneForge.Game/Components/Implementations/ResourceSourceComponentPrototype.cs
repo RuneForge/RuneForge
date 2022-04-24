e5564e9e6ff7bfcd282537623278748c20bf5a21
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 00:33:30 +0300

    Add the ResourceSourceComponentPrototype class

---
+++ Source/RuneForge.Game/Components/Implementations/ResourceSourceComponentPrototype.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using RuneForge.Game.Components.Entities;
+
+namespace RuneForge.Game.Components.Implementations
+{
+    public class ResourceSourceComponentPrototype : ComponentPrototype
+    {
+        public ResourceTypes ResourceType { get; }
+
+        public decimal AmountGiven { get; }
+
+        public TimeSpan ExtractionTime { get; }
+
+        public ResourceSourceComponentPrototype(ResourceTypes resourceType, decimal amountGiven, TimeSpan extractionTime)
+        {
+            ResourceType = resourceType;
+            AmountGiven = amountGiven;
+            ExtractionTime = extractionTime;
+        }
+    }
+}
