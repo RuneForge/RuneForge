deb4ec10e06bee8e2a3ee8c77280055aba9ce5ed
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 00:52:42 +0300

    Add the ResourceSourceComponentPrototypeWriter class

--- Source/RuneForge.Content.Pipeline.Game/Components/Implementations/ResourceSourceComponentPrototype.cs
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/ResourceSourceComponentPrototype.cs
@@ -1,9 +1,12 @@
 ﻿using System.Xml.Serialization;
 
+using RuneForge.Content.Pipeline.Game.Components.Attributes;
+using RuneForge.Content.Pipeline.Game.Components.PrototypeWriters;
 using RuneForge.Game.Components.Entities;
 
 namespace RuneForge.Content.Pipeline.Game.Components.Implementations
 {
+    [ComponentPrototypeWriter(typeof(ResourceSourceComponentPrototypeWriter))]
     public class ResourceSourceComponentPrototype : ComponentPrototype
     {
         private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.ResourceSourceComponentPrototype, RuneForge.Game";
---
+++ Source/RuneForge.Content.Pipeline.Game/Components/PrototypeWriters/ResourceSourceComponentPrototypeWriter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+
+using RuneForge.Content.Pipeline.Game.Components.Implementations;
+
+namespace RuneForge.Content.Pipeline.Game.Components.PrototypeWriters
+{
+    public class ResourceSourceComponentPrototypeWriter : ComponentPrototypeWriter<ResourceSourceComponentPrototype>
+    {
+        public override void WriteComponentPrototype(ContentWriter contentWriter, ResourceSourceComponentPrototype componentPrototype)
+        {
+            contentWriter.Write((int)componentPrototype.ResourceType);
+            contentWriter.Write(componentPrototype.AmountGiven);
+            contentWriter.Write(componentPrototype.ExtractionTimeMilliseconds);
+        }
+    }
+}
