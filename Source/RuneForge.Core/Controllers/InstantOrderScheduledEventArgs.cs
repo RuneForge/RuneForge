16e734131786090eb4e076758e784e5a2a7b71b1
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-24 19:02:41 +0300

    Add the InstantOrderScheduledEventArgs event args class

---
+++ Source/RuneForge.Core/Controllers/InstantOrderScheduledEventArgs.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RuneForge.Core.Controllers
+{
+    public class InstantOrderScheduledEventArgs : EventArgs
+    {
+        public Type OrderType { get; }
+
+        public InstantOrderScheduledEventArgs(Type orderType)
+        {
+            OrderType = orderType;
+        }
+    }
+}
