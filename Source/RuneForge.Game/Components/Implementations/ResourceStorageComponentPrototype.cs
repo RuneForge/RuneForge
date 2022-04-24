3c91ab14b3b900e734143e3891e66f6f3610cc9d
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 01:25:03 +0300

    Add the ResourceStorageComponentPrototype class

---
+++ Source/RuneForge.Game/Components/Implementations/ResourceStorageComponentPrototype.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using RuneForge.Game.Components.Entities;
+
+namespace RuneForge.Game.Components.Implementations
+{
+    public class ResourceStorageComponentPrototype : ComponentPrototype
+    {
+        public ResourceTypes AcceptedResourceTypes { get; }
+
+        public TimeSpan TransferTime { get; }
+
+        public ResourceStorageComponentPrototype(ResourceTypes acceptedResourceTypes, TimeSpan transferTime)
+        {
+            AcceptedResourceTypes = acceptedResourceTypes;
+            TransferTime = transferTime;
+        }
+    }
+}
