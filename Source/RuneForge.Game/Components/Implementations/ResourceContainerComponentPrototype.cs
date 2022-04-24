78c482468eab2dfcdcdac5587a1f056c6f25215c
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-24 23:50:01 +0300

    Add the ResourceContainerComponentPrototype class

---
+++ Source/RuneForge.Game/Components/Implementations/ResourceContainerComponentPrototype.cs
@@ -0,0 +1,12 @@
+﻿namespace RuneForge.Game.Components.Implementations
+{
+    public class ResourceContainerComponentPrototype : ComponentPrototype
+    {
+        public decimal GoldAmount { get; }
+
+        public ResourceContainerComponentPrototype(decimal goldAmount)
+        {
+            GoldAmount = goldAmount;
+        }
+    }
+}
