7dd767d6a12be83ae59c9e2374378e1a8e0c04c5
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 00:40:12 +0300

    Add the ResourceSourceComponentDto data transfer class

---
+++ Source/RuneForge.Data/Components/ResourceSourceComponentDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace RuneForge.Data.Components
+{
+    public class ResourceSourceComponentDto : ComponentDto
+    {
+        public int ResourceType { get; set; }
+
+        public decimal AmountGiven { get; set; }
+
+        public TimeSpan ExtractionTime { get; set; }
+    }
+}
