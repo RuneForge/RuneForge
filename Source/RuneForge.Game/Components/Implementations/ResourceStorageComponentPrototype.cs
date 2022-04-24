104d3674973247f2f608026f11271208ac5595f9
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 01:28:43 +0300

    Add the ResourceStorageComponentPrototypeReader class

--- Source/RuneForge.Game/Components/Implementations/ResourceStorageComponentPrototype.cs
+++ Source/RuneForge.Game/Components/Implementations/ResourceStorageComponentPrototype.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 
+using RuneForge.Game.Components.Attributes;
 using RuneForge.Game.Components.Entities;
+using RuneForge.Game.Components.PrototypeReaders;
 
 namespace RuneForge.Game.Components.Implementations
 {
+    [ComponentPrototypeReader(typeof(ResourceStorageComponentPrototypeReader))]
     public class ResourceStorageComponentPrototype : ComponentPrototype
     {
         public ResourceTypes AcceptedResourceTypes { get; }
---
+++ Source/RuneForge.Game/Components/PrototypeReaders/ResourceStorageComponentPrototypeReader.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+using Microsoft.Xna.Framework.Content;
+
+using RuneForge.Game.Components.Entities;
+using RuneForge.Game.Components.Implementations;
+
+namespace RuneForge.Game.Components.PrototypeReaders
+{
+    public class ResourceStorageComponentPrototypeReader : ComponentPrototypeReader<ResourceStorageComponentPrototype>
+    {
+        public override ResourceStorageComponentPrototype ReadTypedComponentPrototype(ContentReader contentReader)
+        {
+            ResourceTypes acceptedResourceTypes = (ResourceTypes)contentReader.ReadInt32();
+            float transferTimeMilliseconds = contentReader.ReadSingle();
+            return new ResourceStorageComponentPrototype(acceptedResourceTypes, TimeSpan.FromMilliseconds(transferTimeMilliseconds));
+        }
+    }
+}
