f0b568b88f3074ac9b630bc0bf1bf636eca0f0dd
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 13:41:46 +0300

    Add the UnitShelterComponentPrototype class

---
+++ Source/RuneForge.Game/Components/Implementations/UnitShelterComponentPrototype.cs
@@ -0,0 +1,12 @@
+﻿namespace RuneForge.Game.Components.Implementations
+{
+    public class UnitShelterComponentPrototype : ComponentPrototype
+    {
+        public int OccupantsLimit { get; }
+
+        public UnitShelterComponentPrototype(int occupantsLimit)
+        {
+            OccupantsLimit = occupantsLimit;
+        }
+    }
+}
