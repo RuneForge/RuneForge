360644f1b1a112b41527f6311668be57012f8c6c
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 13:57:31 +0300

    Add the UnitShelterComponentPrototypeReader class

--- Source/RuneForge.Game/Components/Implementations/UnitShelterComponentPrototype.cs
+++ Source/RuneForge.Game/Components/Implementations/UnitShelterComponentPrototype.cs
@@ -1,5 +1,9 @@
-﻿namespace RuneForge.Game.Components.Implementations
+﻿using RuneForge.Game.Components.Attributes;
+using RuneForge.Game.Components.PrototypeReaders;
+
+namespace RuneForge.Game.Components.Implementations
 {
+    [ComponentPrototypeReader(typeof(UnitShelterComponentPrototypeReader))]
     public class UnitShelterComponentPrototype : ComponentPrototype
     {
         public int OccupantsLimit { get; }
