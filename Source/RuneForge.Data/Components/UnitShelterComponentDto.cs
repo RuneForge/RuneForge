8dce7a78b3fb82afc39f52aee15506f201b20b02
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 14:01:40 +0300

    Add the UnitShelterComponentDto data transfer class

---
+++ Source/RuneForge.Data/Components/UnitShelterComponentDto.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace RuneForge.Data.Components
+{
+    public class UnitShelterComponentDto : ComponentDto
+    {
+        public int OccupantsLimit { get; set; }
+
+        public List<int> OccupantIds { get; set; }
+    }
+}
