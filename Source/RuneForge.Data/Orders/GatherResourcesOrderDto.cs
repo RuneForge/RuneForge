4824ca233c65ee981f7c6258dd3d40b871579a53
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 22:04:05 +0300

    Add the GatherResourcesOrderDto data transfer class

---
+++ Source/RuneForge.Data/Orders/GatherResourcesOrderDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RuneForge.Data.Orders
+{
+    public class GatherResourcesOrderDto : OrderDto
+    {
+        public int ResourceSourceId { get; set; }
+
+        public bool CancellationRequested { get; set; }
+    }
+}
