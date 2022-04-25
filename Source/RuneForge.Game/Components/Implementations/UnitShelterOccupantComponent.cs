eaecd390b39bfa0d44ac6d396856e320f7d4639e
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 15:11:37 +0300

    Add the UnitShelterOccupantComponent class

---
+++ Source/RuneForge.Game/Components/Implementations/UnitShelterOccupantComponent.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using Microsoft.Xna.Framework;
+
+namespace RuneForge.Game.Components.Implementations
+{
+    public class UnitShelterOccupantComponent : Component
+    {
+        public bool InsideShelter { get; set; }
+
+        public TimeSpan TimeSinceEntering { get; set; }
+
+        public int EnteredFromX { get; set; }
+        public int EnteredFromY { get; set; }
+
+        public UnitShelterOccupantComponent()
+        {
+            InsideShelter = false;
+            TimeSinceEntering = TimeSpan.Zero;
+            EnteredFromX = 0;
+            EnteredFromY = 0;
+        }
+
+        public Point EnteredFrom
+        {
+            get => new Point(EnteredFromX, EnteredFromY);
+            set => (EnteredFromX, EnteredFromY) = value;
+        }
+    }
+}
