9cc07b6d0e54590fd6cf32e42ec0508cea685c7e
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 15:28:46 +0300

    Add the UnitShelterOccupantComponentPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/UnitShelterOccupantComponentPrototype.cs
@@ -0,0 +1,12 @@
+﻿namespace RuneForge.Content.Pipeline.Game.Components.Implementations
+{
+    public class UnitShelterOccupantComponentPrototype : ComponentPrototype
+    {
+        private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.UnitShelterOccupantComponentPrototype, RuneForge.Game";
+
+        public override string GetRuntimeTypeName()
+        {
+            return c_runtimeTypeName;
+        }
+    }
+}
