80d681ced17028227503a7e25ec4bb8979932e7f
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 14:13:21 +0300

    Add the UnitShelterComponentPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/UnitShelterComponentPrototype.cs
@@ -0,0 +1,17 @@
+﻿using System.Xml.Serialization;
+
+namespace RuneForge.Content.Pipeline.Game.Components.Implementations
+{
+    public class UnitShelterComponentPrototype : ComponentPrototype
+    {
+        private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.UnitShelterComponentPrototype, RuneForge.Game";
+
+        [XmlAttribute(AttributeName = "OccupantsLimit")]
+        public int OccupantsLimit { get; set; }
+
+        public override string GetRuntimeTypeName()
+        {
+            return c_runtimeTypeName;
+        }
+    }
+}
