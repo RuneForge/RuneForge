b7a909f6da76fde407b32ab35b784a5d8e5e03c7
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 14:15:31 +0300

    Add the UnitShelterComponentPrototypeWriter class

---
+++ Source/RuneForge.Content.Pipeline.Game/Components/PrototypeWriters/UnitShelterComponentPrototypeWriter.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+
+using RuneForge.Content.Pipeline.Game.Components.Implementations;
+
+namespace RuneForge.Content.Pipeline.Game.Components.PrototypeWriters
+{
+    public class UnitShelterComponentPrototypeWriter : ComponentPrototypeWriter<UnitShelterComponentPrototype>
+    {
+        public override void WriteComponentPrototype(ContentWriter contentWriter, UnitShelterComponentPrototype componentPrototype)
+        {
+            contentWriter.Write(componentPrototype.OccupantsLimit);
+        }
+    }
+}
