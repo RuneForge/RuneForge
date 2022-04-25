cf23d3e59ebb49f382c32fe6a5c4711b0a6858cb
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 15:29:57 +0300

    Add the UnitShelterOccupantComponentPrototypeWriter class

--- Source/RuneForge.Content.Pipeline.Game/Components/Implementations/UnitShelterOccupantComponentPrototype.cs
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/UnitShelterOccupantComponentPrototype.cs
@@ -1,5 +1,9 @@
-﻿namespace RuneForge.Content.Pipeline.Game.Components.Implementations
+﻿using RuneForge.Content.Pipeline.Game.Components.Attributes;
+using RuneForge.Content.Pipeline.Game.Components.PrototypeWriters;
+
+namespace RuneForge.Content.Pipeline.Game.Components.Implementations
 {
+    [ComponentPrototypeWriter(typeof(UnitShelterOccupantComponentPrototypeWriter))]
     public class UnitShelterOccupantComponentPrototype : ComponentPrototype
     {
         private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.UnitShelterOccupantComponentPrototype, RuneForge.Game";
