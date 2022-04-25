903bc0f9a7d5578ea223b3b4b88dd1bb0dc84f27
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-25 13:59:27 +0300

    Add the UnitShelterComponentFactory class

---
+++ Source/RuneForge.Game/Components/Factories/UnitShelterComponentFactory.cs
@@ -0,0 +1,12 @@
+﻿using RuneForge.Game.Components.Implementations;
+
+namespace RuneForge.Game.Components.Factories
+{
+    public class UnitShelterComponentFactory : ComponentFactory<UnitShelterComponent, UnitShelterComponentPrototype>
+    {
+        public override UnitShelterComponent CreateComponentFromPrototype(UnitShelterComponentPrototype componentPrototype, UnitShelterComponentPrototype componentPrototypeOverride)
+        {
+            return new UnitShelterComponent(componentPrototype.OccupantsLimit);
+        }
+    }
+}
--- Source/RuneForge.Game/Components/Implementations/UnitShelterComponentPrototype.cs
+++ Source/RuneForge.Game/Components/Implementations/UnitShelterComponentPrototype.cs
@@ -1,8 +1,10 @@
 ﻿using RuneForge.Game.Components.Attributes;
+using RuneForge.Game.Components.Factories;
 using RuneForge.Game.Components.PrototypeReaders;
 
 namespace RuneForge.Game.Components.Implementations
 {
+    [ComponentFactory(typeof(UnitShelterComponentFactory))]
     [ComponentPrototypeReader(typeof(UnitShelterComponentPrototypeReader))]
     public class UnitShelterComponentPrototype : ComponentPrototype
     {
