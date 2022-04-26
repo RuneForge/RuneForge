d0ef2b3fc2015596ebcf04e1483287102f8972ea
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-27 00:00:56 +0300

    Add the MeleeCombatComponentPrototypeWriter class

--- Source/RuneForge.Content.Pipeline.Game/Components/Implementations/MeleeCombatComponentPrototype.cs
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/MeleeCombatComponentPrototype.cs
@@ -1,5 +1,9 @@
-﻿namespace RuneForge.Content.Pipeline.Game.Components.Implementations
+﻿using RuneForge.Content.Pipeline.Game.Components.Attributes;
+using RuneForge.Content.Pipeline.Game.Components.PrototypeWriters;
+
+namespace RuneForge.Content.Pipeline.Game.Components.Implementations
 {
+    [ComponentPrototypeWriter(typeof(MeleeCombatComponentPrototypeWriter))]
     public class MeleeCombatComponentPrototype : ComponentPrototype
     {
         private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.MeleeCombatComponentPrototype, RuneForge.Game";
