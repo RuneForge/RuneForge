e739e896324f515bda7721df104d954ccd273e65
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-26 23:58:08 +0300

    Add the MeleeCombatComponentPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/MeleeCombatComponentPrototype.cs
@@ -0,0 +1,12 @@
+﻿namespace RuneForge.Content.Pipeline.Game.Components.Implementations
+{
+    public class MeleeCombatComponentPrototype : ComponentPrototype
+    {
+        private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.MeleeCombatComponentPrototype, RuneForge.Game";
+
+        public override string GetRuntimeTypeName()
+        {
+            return c_runtimeTypeName;
+        }
+    }
+}
