3bb44610686cc8ef30557deaa8aeb3da0f01f8ea
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-26 23:53:28 +0300

    Add the MeleeCombatComponentFactory class

---
+++ Source/RuneForge.Game/Components/Factories/MeleeCombatComponentFactory.cs
@@ -0,0 +1,12 @@
+﻿using RuneForge.Game.Components.Implementations;
+
+namespace RuneForge.Game.Components.Factories
+{
+    public class MeleeCombatComponentFactory : ComponentFactory<MeleeCombatComponent, MeleeCombatComponentPrototype>
+    {
+        public override MeleeCombatComponent CreateComponentFromPrototype(MeleeCombatComponentPrototype componentPrototype, MeleeCombatComponentPrototype componentPrototypeOverride)
+        {
+            return new MeleeCombatComponent();
+        }
+    }
+}
--- Source/RuneForge.Game/Components/Implementations/MeleeCombatComponentPrototype.cs
+++ Source/RuneForge.Game/Components/Implementations/MeleeCombatComponentPrototype.cs
@@ -1,8 +1,10 @@
 ﻿using RuneForge.Game.Components.Attributes;
+using RuneForge.Game.Components.Factories;
 using RuneForge.Game.Components.PrototypeReaders;
 
 namespace RuneForge.Game.Components.Implementations
 {
+    [ComponentFactory(typeof(MeleeCombatComponentFactory))]
     [ComponentPrototypeReader(typeof(MeleeCombatComponentPrototypeReader))]
     public class MeleeCombatComponentPrototype : ComponentPrototype
     {
