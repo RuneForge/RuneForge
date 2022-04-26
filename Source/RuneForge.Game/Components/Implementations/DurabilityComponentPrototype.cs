75d3d6648e572bc09695be5046e35736e41a373c
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-26 23:06:57 +0300

    Add the DurabilityComponentPrototype class

---
+++ Source/RuneForge.Game/Components/Implementations/DurabilityComponentPrototype.cs
@@ -0,0 +1,15 @@
+﻿namespace RuneForge.Game.Components.Implementations
+{
+    public class DurabilityComponentPrototype : ComponentPrototype
+    {
+        public decimal Durability { get; }
+
+        public decimal MaxDurability { get; }
+
+        public DurabilityComponentPrototype(decimal durability, decimal maxDurability)
+        {
+            Durability = durability;
+            MaxDurability = durability;
+        }
+    }
+}
