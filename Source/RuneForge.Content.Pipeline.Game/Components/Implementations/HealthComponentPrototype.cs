cfe908cce8998c2eedb06f0128daf8bb1fdb2e9a
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-26 22:28:58 +0300

    Add the HealthComponentPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/HealthComponentPrototype.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace RuneForge.Content.Pipeline.Game.Components.Implementations
+{
+    public class HealthComponentPrototype : ComponentPrototype
+    {
+        private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.HealthComponentPrototype, RuneForge.Game";
+
+        [XmlAttribute(AttributeName = "Health")]
+        public decimal Health { get; set; }
+
+        [XmlAttribute(AttributeName = "MaxHealth")]
+        public decimal MaxHealth { get; set; }
+
+        public override string GetRuntimeTypeName()
+        {
+            return c_runtimeTypeName;
+        }
+    }
+}
