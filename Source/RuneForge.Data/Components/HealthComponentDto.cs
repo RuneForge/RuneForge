f2b5d41fc17bb873e0c00ee54bf57cc51e7e0891
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-26 22:21:22 +0300

    Add the HealthComponentDto data transfer class

---
+++ Source/RuneForge.Data/Components/HealthComponentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RuneForge.Data.Components
+{
+    public class HealthComponentDto : ComponentDto
+    {
+        public decimal Health { get; set; }
+
+        public decimal MaxHealth { get; set; }
+    }
+}
