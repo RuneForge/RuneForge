dfb2c6bcaf02fc9dd510dd9dd2ea1fb50aa86263
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-26 23:50:26 +0300

    Add the MeleeCombatComponentPrototypeReader class

---
+++ Source/RuneForge.Game/Components/PrototypeReaders/MeleeCombatComponentPrototypeReader.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework.Content;
+
+using RuneForge.Game.Components.Implementations;
+
+namespace RuneForge.Game.Components.PrototypeReaders
+{
+    public class MeleeCombatComponentPrototypeReader : ComponentPrototypeReader<MeleeCombatComponentPrototype>
+    {
+        public override MeleeCombatComponentPrototype ReadTypedComponentPrototype(ContentReader contentReader)
+        {
+            return new MeleeCombatComponentPrototype();
+        }
+    }
+}
