22da4a59072dc6dc72f80ae54e0374e704e54f86
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-26 22:19:34 +0300

    Add the HealthComponentFactory class

---
+++ Source/RuneForge.Game/Components/Factories/HealthComponentFactory.cs
@@ -0,0 +1,16 @@
+﻿using RuneForge.Game.Components.Implementations;
+
+namespace RuneForge.Game.Components.Factories
+{
+    public class HealthComponentFactory : ComponentFactory<HealthComponent, HealthComponentPrototype>
+    {
+        public override HealthComponent CreateComponentFromPrototype(HealthComponentPrototype componentPrototype, HealthComponentPrototype componentPrototypeOverride)
+        {
+            return new HealthComponent()
+            {
+                Health = componentPrototype.Health,
+                MaxHealth = componentPrototype.MaxHealth,
+            };
+        }
+    }
+}
