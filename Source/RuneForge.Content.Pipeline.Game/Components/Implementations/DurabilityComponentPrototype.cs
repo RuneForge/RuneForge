a4f0d8a44c16648e246e07c5d8aa3c5250b62e1f
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-26 23:19:38 +0300

    Add the DurabilityComponentPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/DurabilityComponentPrototype.cs
@@ -0,0 +1,20 @@
+﻿using System.Xml.Serialization;
+
+namespace RuneForge.Content.Pipeline.Game.Components.Implementations
+{
+    public class DurabilityComponentPrototype : ComponentPrototype
+    {
+        private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.DurabilityComponentPrototype, RuneForge.Game";
+
+        [XmlAttribute(AttributeName = "Durability")]
+        public decimal Durability { get; set; }
+
+        [XmlAttribute(AttributeName = "MaxDurability")]
+        public decimal MaxDurability { get; set; }
+
+        public override string GetRuntimeTypeName()
+        {
+            return c_runtimeTypeName;
+        }
+    }
+}
