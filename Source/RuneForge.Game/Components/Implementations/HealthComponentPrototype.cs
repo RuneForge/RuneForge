126bad768b2627d185e7ecea6ae73bb7099a3a6c
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-26 22:12:51 +0300

    Add the HealthComponentPrototype class

---
+++ Source/RuneForge.Game/Components/Implementations/HealthComponentPrototype.cs
@@ -0,0 +1,15 @@
+﻿namespace RuneForge.Game.Components.Implementations
+{
+    public class HealthComponentPrototype : ComponentPrototype
+    {
+        public decimal Health { get; }
+
+        public decimal MaxHealth { get; }
+
+        public HealthComponentPrototype(decimal health, decimal maxHealth)
+        {
+            Health = health;
+            MaxHealth = maxHealth;
+        }
+    }
+}
