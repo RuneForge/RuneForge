efa37ad5c14c4c48916806fd460107b42a95b33b
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-26 19:19:25 +0300

    Implement 'gather resources' order support for the OrderTypeResolver class

--- Source/RuneForge.Core/Controllers/OrderTypeResolver.cs
+++ Source/RuneForge.Core/Controllers/OrderTypeResolver.cs
@@ -1,10 +1,12 @@
 ﻿using System;
+using System.Linq;
 
 using Microsoft.Xna.Framework;
 
 using RuneForge.Core.Controllers.Interfaces;
 using RuneForge.Core.Rendering;
 using RuneForge.Game.Buildings;
+using RuneForge.Game.Components.Implementations;
 using RuneForge.Game.Entities;
 using RuneForge.Game.GameSessions.Interfaces;
 using RuneForge.Game.Maps;
@@ -44,6 +46,17 @@ namespace RuneForge.Core.Controllers
 
             if (entity is Unit)
             {
+                Entity[] entitiesByLocation = GetEntitiesByLocation(worldPoint);
+
+                if (entitiesByLocation.Any(targetEntity => targetEntity is Building
+                    && targetEntity.TryGetComponentOfType(out ResourceSourceComponent resourceSourceComponent)
+                    && targetEntity.TryGetComponentOfType(out ResourceContainerComponent resourceContainerComponent)
+                    && resourceContainerComponent.GetResourceAmount(resourceSourceComponent.ResourceType) > 0))
+                {
+                    orderType = typeof(GatherResourcesOrder);
+                    return true;
+                }
+
                 if (worldPoint.X >= 0 && worldPoint.Y >= 0 && worldPoint.X < m_gameSessionContext.Map.Width * Map.CellWidth && worldPoint.Y < m_gameSessionContext.Map.Height * Map.CellHeight)
                 {
                     orderType = typeof(MoveOrder);
@@ -56,6 +69,16 @@ namespace RuneForge.Core.Controllers
 
             orderType = null;
             return false;
+        }
+
+        private Entity[] GetEntitiesByLocation(Point worldPoint)
+        {
+            return m_gameSessionContext.Units.Concat<Entity>(m_gameSessionContext.Buildings)
+                .Where(entity => entity.TryGetComponentOfType(out LocationComponent locationComponent)
+                    && new Rectangle((int)locationComponent.X, (int)locationComponent.Y, locationComponent.Width, locationComponent.Height).Contains(worldPoint)
+                    && (!entity.TryGetComponentOfType(out UnitShelterOccupantComponent shelterOccupantComponent)
+                    || !shelterOccupantComponent.InsideShelter))
+                .ToArray();
         }
     }
 }
