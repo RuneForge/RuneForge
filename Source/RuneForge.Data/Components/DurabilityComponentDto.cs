12e972ff2b152fdff0859039918a87560bdf4389
Andrey Talanin <andrey.talanin@outlook.com>
2022-04-26 23:15:35 +0300

    Add the DurabilityComponentDto data transfer class

---
+++ Source/RuneForge.Data/Components/DurabilityComponentDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RuneForge.Data.Components
+{
+    public class DurabilityComponentDto : ComponentDto
+    {
+        public decimal Durability { get; set; }
+
+        public decimal MaxDurability { get; set; }
+    }
+}
