65c9bb7372eb9ce73741288ca9dc06ebbfc7281b
Andrey Talanin <andrey.talanin@outlook.com>
2022-05-01 23:40:07 +0300

    Add the AttackOrderDto data transfer class to the list of mappings

---
+++ Source/RuneForge.Game/AutoMapper/Resolvers/AttackOrderTargetEntityIdValueResolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+using AutoMapper;
+
+using RuneForge.Data.Orders;
+using RuneForge.Game.Buildings;
+using RuneForge.Game.Orders.Implementations;
+using RuneForge.Game.Units;
+
+namespace RuneForge.Game.AutoMapper.Resolvers
+{
+    public class AttackOrderTargetEntityIdValueResolver : IValueResolver<AttackOrder, AttackOrderDto, string>
+    {
+        public string Resolve(AttackOrder source, AttackOrderDto destination, string destMember, ResolutionContext context)
+        {
+            if (source.TargetEntity == null)
+                return null;
+            else
+            {
+                string typeName = source.TargetEntity.GetType().Name;
+                int id = source.TargetEntity switch
+                {
+                    Unit unit => unit.Id,
+                    Building building => building.Id,
+                    _ => throw new NotSupportedException($"Unable to resaolve an integer Id for the entity of type {typeName}"),
+                };
+                return $"{typeName}:{id}";
+            }
+        }
+    }
+}
