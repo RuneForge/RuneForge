c6e9e286243205bb082756e229a7f86dfd1d6a63
Andrey Talanin <andrey.talanin@outlook.com>
2022-05-03 21:32:45 +0300

    Add the ProductionFacilityComponentPrototypeWriter class

--- Source/RuneForge.Content.Pipeline.Game/Components/Implementations/ProductionFacilityComponentPrototype.cs
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/ProductionFacilityComponentPrototype.cs
@@ -1,8 +1,12 @@
 ﻿using System.Collections.Generic;
 using System.Xml.Serialization;
 
+using RuneForge.Content.Pipeline.Game.Components.Attributes;
+using RuneForge.Content.Pipeline.Game.Components.PrototypeWriters;
+
 namespace RuneForge.Content.Pipeline.Game.Components.Implementations
 {
+    [ComponentPrototypeWriter(typeof(ProductionFacilityComponentPrototypeWriter))]
     public class ProductionFacilityComponentPrototype : ComponentPrototype
     {
         private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.ProductionFacilityComponentPrototype, RuneForge.Game";
