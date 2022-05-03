f7ebaf445fca6e30d7c65a88e1bd331801611094
Andrey Talanin <andrey.talanin@outlook.com>
2022-05-03 20:27:31 +0300

    Add the ProductionFacilityComponentPrototype class

---
+++ Source/RuneForge.Game/Components/Implementations/ProductionFacilityComponentPrototype.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace RuneForge.Game.Components.Implementations
+{
+    public class ProductionFacilityComponentPrototype : ComponentPrototype
+    {
+        public ReadOnlyCollection<string> UnitCodesProduced { get; }
+
+        public ProductionFacilityComponentPrototype(IList<string> unitCodesProduced)
+        {
+            UnitCodesProduced = new ReadOnlyCollection<string>(unitCodesProduced);
+        }
+    }
+}
