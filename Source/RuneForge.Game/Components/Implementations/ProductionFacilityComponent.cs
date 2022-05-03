2304ec6bb0dd8a603903a68c3745fc707999556a
Andrey Talanin <andrey.talanin@outlook.com>
2022-05-03 20:25:39 +0300

    Add the ProductionFacilityComponent class

---
+++ Source/RuneForge.Game/Components/Implementations/ProductionFacilityComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+using RuneForge.Game.Units;
+
+namespace RuneForge.Game.Components.Implementations
+{
+    public class ProductionFacilityComponent : Component
+    {
+        public ReadOnlyCollection<UnitPrototype> UnitsProduced { get; }
+
+        public UnitPrototype UnitCurrentlyProduced { get; set; }
+
+        public TimeSpan TimeElapsed { get; set; }
+
+        public bool ProductionFinished { get; set; }
+
+        public ProductionFacilityComponent(IList<UnitPrototype> unitsProduced)
+        {
+            UnitsProduced = new ReadOnlyCollection<UnitPrototype>(unitsProduced);
+            UnitCurrentlyProduced = null;
+            TimeElapsed = TimeSpan.Zero;
+            ProductionFinished = false;
+        }
+    }
+}
