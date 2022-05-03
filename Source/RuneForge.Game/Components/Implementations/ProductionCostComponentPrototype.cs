a5d5d80d2017cea28df566fed09100c42b7077a3
Andrey Talanin <andrey.talanin@outlook.com>
2022-05-03 18:55:06 +0300

    Add the ProductionCostComponentPrototypeReader class

--- Source/RuneForge.Game/Components/Implementations/ProductionCostComponentPrototype.cs
+++ Source/RuneForge.Game/Components/Implementations/ProductionCostComponentPrototype.cs
@@ -1,7 +1,11 @@
 ﻿using System;
 
+using RuneForge.Game.Components.Attributes;
+using RuneForge.Game.Components.PrototypeReaders;
+
 namespace RuneForge.Game.Components.Implementations
 {
+    [ComponentPrototypeReader(typeof(ProductionCostComponentPrototypeReader))]
     public class ProductionCostComponentPrototype : ComponentPrototype
     {
         public decimal GoldAmount { get; }
---
+++ Source/RuneForge.Game/Components/PrototypeReaders/ProductionCostComponentPrototypeReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using Microsoft.Xna.Framework.Content;
+
+using RuneForge.Game.Components.Implementations;
+
+namespace RuneForge.Game.Components.PrototypeReaders
+{
+    public class ProductionCostComponentPrototypeReader : ComponentPrototypeReader<ProductionCostComponentPrototype>
+    {
+        public override ProductionCostComponentPrototype ReadTypedComponentPrototype(ContentReader contentReader)
+        {
+            decimal goldAmount = contentReader.ReadDecimal();
+            TimeSpan productionTime = TimeSpan.FromMilliseconds(contentReader.ReadSingle());
+            return new ProductionCostComponentPrototype(goldAmount, productionTime);
+        }
+    }
+}
