346d3efdc387f071654386274c75e9740104e02c
Andrey Talanin <andrey.talanin@outlook.com>
2022-05-03 18:56:46 +0300

    Add the ProductionCostComponentFactory class

---
+++ Source/RuneForge.Game/Components/Factories/ProductionCostComponentFactory.cs
@@ -0,0 +1,12 @@
+﻿using RuneForge.Game.Components.Implementations;
+
+namespace RuneForge.Game.Components.Factories
+{
+    public class ProductionCostComponentFactory : ComponentFactory<ProductionCostComponent, ProductionCostComponentPrototype>
+    {
+        public override ProductionCostComponent CreateComponentFromPrototype(ProductionCostComponentPrototype componentPrototype, ProductionCostComponentPrototype componentPrototypeOverride)
+        {
+            return new ProductionCostComponent(componentPrototype.GoldAmount, componentPrototype.ProductionTime);
+        }
+    }
+}
--- Source/RuneForge.Game/Components/Implementations/ProductionCostComponentPrototype.cs
+++ Source/RuneForge.Game/Components/Implementations/ProductionCostComponentPrototype.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 
 using RuneForge.Game.Components.Attributes;
+using RuneForge.Game.Components.Factories;
 using RuneForge.Game.Components.PrototypeReaders;
 
 namespace RuneForge.Game.Components.Implementations
 {
+    [ComponentFactory(typeof(ProductionCostComponentFactory))]
     [ComponentPrototypeReader(typeof(ProductionCostComponentPrototypeReader))]
     public class ProductionCostComponentPrototype : ComponentPrototype
     {
