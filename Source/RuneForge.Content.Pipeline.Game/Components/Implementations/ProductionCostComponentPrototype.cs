6b4252020e3fd7ebf36c737d85f43bff4675a4ef
Andrey Talanin <andrey.talanin@outlook.com>
2022-05-03 19:05:45 +0300

    Add the ProductionCostComponentPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/ProductionCostComponentPrototype.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace RuneForge.Content.Pipeline.Game.Components.Implementations
+{
+    public class ProductionCostComponentPrototype : ComponentPrototype
+    {
+        private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.ProductionCostComponentPrototype, RuneForge.Game";
+
+        [XmlAttribute(AttributeName = "GoldAmount")]
+        public decimal GoldAmount { get; set; }
+
+        [XmlAttribute(AttributeName = "ProductionTime")]
+        public float ProductionTimeMilliseconds { get; set; }
+
+        public override string GetRuntimeTypeName()
+        {
+            return c_runtimeTypeName;
+        }
+    }
+}
