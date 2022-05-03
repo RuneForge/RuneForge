deb2bfdfdb8ee842152c5cc9abba9387668fda3e
Andrey Talanin <andrey.talanin@outlook.com>
2022-05-03 18:58:03 +0300

    Add the ProductionCostComponentDto data transfer class

---
+++ Source/RuneForge.Data/Components/ProductionCostComponentDto.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace RuneForge.Data.Components
+{
+    public class ProductionCostComponentDto : ComponentDto
+    {
+        public decimal GoldAmount { get; set; }
+
+        public TimeSpan ProductionTime { get; set; }
+    }
+}
