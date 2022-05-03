bb38e5439213588d97fc04376801d0b2dd2d4a54
Andrey Talanin <andrey.talanin@outlook.com>
2022-05-03 19:16:42 +0300

    Add the ProductionCostComponentPrototypeWriter class

--- Source/RuneForge.Content.Pipeline.Game/Components/Implementations/ProductionCostComponentPrototype.cs
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/ProductionCostComponentPrototype.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Xml.Serialization;
+﻿using System.Xml.Serialization;
+
+using RuneForge.Content.Pipeline.Game.Components.Attributes;
+using RuneForge.Content.Pipeline.Game.Components.PrototypeWriters;
 
 namespace RuneForge.Content.Pipeline.Game.Components.Implementations
 {
+    [ComponentPrototypeWriter(typeof(ProductionCostComponentPrototypeWriter))]
     public class ProductionCostComponentPrototype : ComponentPrototype
     {
         private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.ProductionCostComponentPrototype, RuneForge.Game";
---
+++ Source/RuneForge.Content.Pipeline.Game/Components/PrototypeWriters/ProductionCostComponentPrototypeWriter.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework.Content.Pipeline.Serialization.Compiler;
+
+using RuneForge.Content.Pipeline.Game.Components.Implementations;
+
+namespace RuneForge.Content.Pipeline.Game.Components.PrototypeWriters
+{
+    public class ProductionCostComponentPrototypeWriter : ComponentPrototypeWriter<ProductionCostComponentPrototype>
+    {
+        public override void WriteComponentPrototype(ContentWriter contentWriter, ProductionCostComponentPrototype componentPrototype)
+        {
+            contentWriter.Write(componentPrototype.GoldAmount);
+            contentWriter.Write(componentPrototype.ProductionTimeMilliseconds);
+        }
+    }
+}
