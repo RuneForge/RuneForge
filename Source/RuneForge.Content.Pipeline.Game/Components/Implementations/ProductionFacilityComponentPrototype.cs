0fba6b2e2d790e5209910649c48bacc69967ff53
Andrey Talanin <andrey.talanin@outlook.com>
2022-05-03 21:29:02 +0300

    Add the ProductionFacilityComponentPrototype data transfer class

---
+++ Source/RuneForge.Content.Pipeline.Game/Components/Implementations/ProductionFacilityComponentPrototype.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace RuneForge.Content.Pipeline.Game.Components.Implementations
+{
+    public class ProductionFacilityComponentPrototype : ComponentPrototype
+    {
+        private const string c_runtimeTypeName = "RuneForge.Game.Components.Implementations.ProductionFacilityComponentPrototype, RuneForge.Game";
+
+        [XmlArray(ElementName = "UnitCodesProduced")]
+        [XmlArrayItem(ElementName = "UnitCodeProduced")]
+        public List<string> UnitCodesProduced { get; set; }
+
+        public override string GetRuntimeTypeName()
+        {
+            return c_runtimeTypeName;
+        }
+    }
+}
