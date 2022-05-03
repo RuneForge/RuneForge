7147a1655754e5dde2589bb849fc94099115d781
Andrey Talanin <andrey.talanin@outlook.com>
2022-05-03 17:54:25 +0300

    Add the ProductionCostComponentPrototype class

---
+++ Source/RuneForge.Game/Components/Implementations/ProductionCostComponentPrototype.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace RuneForge.Game.Components.Implementations
+{
+    public class ProductionCostComponentPrototype : ComponentPrototype
+    {
+        public decimal GoldAmount { get; }
+
+        public TimeSpan ProductionTime { get; }
+
+        public ProductionCostComponentPrototype(decimal goldAmount, TimeSpan productionTime)
+        {
+            GoldAmount = goldAmount;
+            ProductionTime = productionTime;
+        }
+    }
+}
