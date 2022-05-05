b357384db1a300059827d36bdea6192e3b083cd9
Andrey Talanin <andrey.talanin@outlook.com>
2022-05-05 19:28:55 +0300

    Add the OrderFactory class

---
+++ Source/RuneForge.Game/Orders/OrderFactory.cs
@@ -0,0 +1,21 @@
+﻿using RuneForge.Data.Orders;
+using RuneForge.Game.Orders.Interfaces;
+
+namespace RuneForge.Game.Orders
+{
+    public abstract class OrderFactory<TOrder, TOrderDto> : IOrderFactory<TOrder, TOrderDto>
+        where TOrder : Order
+        where TOrderDto : OrderDto
+    {
+        protected OrderFactory()
+        {
+        }
+
+        public abstract TOrder CreateOrderFromDto(TOrderDto orderDto);
+
+        public Order CreateOrderFromDto(OrderDto orderDto)
+        {
+            return CreateOrderFromDto((TOrderDto)orderDto);
+        }
+    }
+}
