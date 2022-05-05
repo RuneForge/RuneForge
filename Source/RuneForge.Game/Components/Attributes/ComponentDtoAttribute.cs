ec67bad1d930964632d42a64c71ffa4479839dbb
Andrey Talanin <andrey.talanin@outlook.com>
2022-05-05 21:39:37 +0300

    Add the ComponentDtoAttribute attribute class

---
+++ Source/RuneForge.Game/Components/Attributes/ComponentDtoAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace RuneForge.Game.Components.Attributes
+{
+    [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
+    public sealed class ComponentDtoAttribute : Attribute
+    {
+        public Type ComponentDtoType { get; }
+
+        public ComponentDtoAttribute(Type componentDtoType)
+        {
+            ComponentDtoType = componentDtoType;
+        }
+    }
+}
