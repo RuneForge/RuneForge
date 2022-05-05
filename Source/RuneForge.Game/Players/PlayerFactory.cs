1783e06ba03376ff9007c44df3191d2e0b10dede
Andrey Talanin <andrey.talanin@outlook.com>
2022-05-05 21:29:55 +0300

    Add the CreateFromDto method to the PlayerFactory class

--- Source/RuneForge.Game/Players/PlayerFactory.cs
+++ Source/RuneForge.Game/Players/PlayerFactory.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.ObjectModel;
 
+using Microsoft.Xna.Framework;
+
+using RuneForge.Data.Players;
 using RuneForge.Game.Components;
 using RuneForge.Game.Components.Interfaces;
 using RuneForge.Game.Players.Interfaces;
@@ -20,6 +23,18 @@ namespace RuneForge.Game.Players
         {
             Collection<IComponent> components = ComponentFactoryHelpers.CreateComponentCollection(m_serviceProvider, prototype.ComponentPrototypes);
             return new Player(prototype.Id, prototype.Name, prototype.Color, components);
+        }
+
+        public Player CreateFromDto(PlayerDto playerDto)
+        {
+            PlayerColor playerColor = new PlayerColor(
+                new Color(playerDto.Color.MainColorPacked),
+                new Color(playerDto.Color.EntityColorShadeAPacked),
+                new Color(playerDto.Color.EntityColorShadeBPacked),
+                new Color(playerDto.Color.EntityColorShadeCPacked),
+                new Color(playerDto.Color.EntityColorShadeDPacked)
+                );
+            return new Player(playerDto.Id, playerDto.Name, playerColor, Array.Empty<IComponent>());
         }
     }
 }
