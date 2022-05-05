3a84ce5e16c8c4a3ab40a32577c1cbea3767d2d0
Andrey Talanin <andrey.talanin@outlook.com>
2022-05-05 21:27:09 +0300

    Implement new game session startup logic in the RuneForgeGame class

--- Source/RuneForge/RuneForgeGame.cs
+++ Source/RuneForge/RuneForgeGame.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
 
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
@@ -8,9 +12,11 @@ using Microsoft.Xna.Framework.Input;
 
 using RuneForge.Configuration;
 using RuneForge.Core.GameStates;
+using RuneForge.Core.GameStates.Implementations;
 using RuneForge.Core.GameStates.Interfaces;
 using RuneForge.Core.Input;
 using RuneForge.Core.Input.EventProviders.Interfaces;
+using RuneForge.Game.GameSessions;
 
 using XnaGame = Microsoft.Xna.Framework.Game;
 
@@ -18,13 +24,19 @@ namespace RuneForge
 {
     public class RuneForgeGame : XnaGame
     {
+        private static readonly string s_defaultMapAssetName = Path.Combine("Maps", "Southshore");
+
+        private readonly IConfiguration m_configuration;
+        private readonly IServiceScopeFactory m_serviceScopeFactory;
         private readonly IGameStateService m_gameStateService;
         private readonly IKeyboardEventProvider m_keyboardEventProvider;
         private readonly Lazy<GraphicsDeviceManager> m_graphicsDeviceManagerProvider;
         private readonly GraphicsConfiguration m_graphicsConfiguration;
 
         public RuneForgeGame(
+            IConfiguration configuration,
             IServiceProvider serviceProvider,
+            IServiceScopeFactory serviceScopeFactory,
             GameWindow gameWindow,
             IGameStateService gameStateService,
             IKeyboardEventProvider keyboardEventProvider,
@@ -34,6 +46,8 @@ namespace RuneForge
             )
             : base(serviceProvider, gameWindow)
         {
+            m_configuration = configuration;
+            m_serviceScopeFactory = serviceScopeFactory;
             m_gameStateService = gameStateService;
             m_keyboardEventProvider = keyboardEventProvider;
             m_graphicsDeviceManagerProvider = graphicsDeviceManagerProvider;
@@ -63,7 +77,32 @@ namespace RuneForge
             GraphicsDevice.PresentationParameters.RenderTargetUsage = RenderTargetUsage.PreserveContents;
             base.LoadContent();
 
-            m_gameStateService.RunGameState<MainMenuGameState>();
+            bool loadSavedGame = m_configuration.GetValue<bool>("LoadGame");
+
+            if (!loadSavedGame)
+                m_gameStateService.RunGameState<MainMenuGameState>();
+            else
+            {
+                bool loadSavedGameInPausedState = m_configuration.GetValue<bool>("LoadGame:LoadPaused");
+                string savedGameFileName = m_configuration.GetValue<string>("LoadGame:FileName");
+
+                if (string.IsNullOrEmpty(savedGameFileName))
+                    throw new InvalidOperationException("No saved game file name was provided.");
+
+                IServiceScope serviceScope = m_serviceScopeFactory.CreateScope();
+                GameSessionParameters gameSessionParameters = serviceScope.ServiceProvider.GetRequiredService<GameSessionParameters>();
+                BinaryFormatter binaryFormatter = new BinaryFormatter();
+
+                using (FileStream fileStream = new FileStream(savedGameFileName, FileMode.Open))
+                {
+                    gameSessionParameters.Type = GameSessionType.LoadedGame;
+                    gameSessionParameters.MapAssetName = s_defaultMapAssetName;
+                    gameSessionParameters.GameSessionContext = (SerializableGameSessionContext)binaryFormatter.Deserialize(fileStream);
+                    gameSessionParameters.StartPaused = loadSavedGameInPausedState;
+                }
+
+                m_gameStateService.RunGameState(serviceScope, serviceScope.ServiceProvider.GetRequiredService<GameplayGameState>());
+            }
         }
 
         protected override void Update(GameTime gameTime)
