0ecddde73c98c9cfc8a3a8e55883f9903430fbde
Andrey Talanin <andrey.talanin@outlook.com>
2022-05-17 18:26:34 +0300

    Add unit tests for the InMemoryUnitRepository and InMemoryBuildingRepository classes

---
+++ Source/Tests/RuneForge.Data.Tests/Buildings/InMemoryBuildingRepositoryTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using RuneForge.Data.Buildings;
+
+namespace RuneForge.Data.Tests.Buildings
+{
+    [TestClass]
+    public class InMemoryBuildingRepositoryTests
+    {
+        private const int c_invalidBuildingId = 0;
+        private const int c_validBuildingId = 1;
+
+        [TestMethod]
+        public void AddBuilding_NullBuildingId_ArgumentExceptionThrown()
+        {
+            InMemoryBuildingRepository buildingRepository = new InMemoryBuildingRepository();
+
+            void Action() => buildingRepository.AddBuilding(new BuildingDto() { Id = c_invalidBuildingId });
+
+            Assert.ThrowsException<ArgumentException>(Action);
+        }
+
+        [TestMethod]
+        public void AddBuilding_ExistingBuildingId_InvalidOperationExceptionThrown()
+        {
+            InMemoryBuildingRepository buildingRepository = new InMemoryBuildingRepository();
+
+            buildingRepository.AddBuilding(new BuildingDto() { Id = c_validBuildingId });
+            void Action() => buildingRepository.AddBuilding(new BuildingDto() { Id = c_validBuildingId });
+
+            Assert.ThrowsException<InvalidOperationException>(Action);
+        }
+
+        [TestMethod]
+        public void AddBuilding_NormalBuilding_BuildingAdded()
+        {
+            InMemoryBuildingRepository buildingRepository = new InMemoryBuildingRepository();
+
+            BuildingDto building = new BuildingDto() { Id = c_validBuildingId };
+            buildingRepository.AddBuilding(building);
+
+            Assert.AreSame(building, buildingRepository.GetBuilding(building.Id));
+        }
+
+        [TestMethod]
+        public void SaveBuilding_NonExistingBuildingId_KeyNotFoundExceptionThrown()
+        {
+            InMemoryBuildingRepository buildingRepository = new InMemoryBuildingRepository();
+
+            void Action() => buildingRepository.SaveBuilding(new BuildingDto() { Id = c_validBuildingId });
+
+            Assert.ThrowsException<KeyNotFoundException>(Action);
+        }
+
+        [TestMethod]
+        public void SaveBuilding_SameBuilding_NoAction()
+        {
+            InMemoryBuildingRepository buildingRepository = new InMemoryBuildingRepository();
+
+            BuildingDto building = new BuildingDto() { Id = c_validBuildingId };
+            buildingRepository.AddBuilding(building);
+            buildingRepository.SaveBuilding(building);
+
+            Assert.AreSame(building, buildingRepository.GetBuilding(building.Id));
+        }
+
+        [TestMethod]
+        public void SaveBuilding_BuildingWithEqualId_BuildingSaved()
+        {
+            InMemoryBuildingRepository buildingRepository = new InMemoryBuildingRepository();
+
+            BuildingDto initialBuilding = new BuildingDto() { Id = c_validBuildingId };
+            BuildingDto finalBuilding = new BuildingDto() { Id = c_validBuildingId };
+            buildingRepository.AddBuilding(initialBuilding);
+            buildingRepository.SaveBuilding(finalBuilding);
+
+            Assert.AreSame(finalBuilding, buildingRepository.GetBuilding(c_validBuildingId));
+        }
+
+        [TestMethod]
+        public void RemoveBuilding_NonExistingBuildingId_KeyNotFoundExceptionThrown()
+        {
+            InMemoryBuildingRepository buildingRepository = new InMemoryBuildingRepository();
+
+            void Action() => buildingRepository.RemoveBuilding(c_validBuildingId);
+
+            Assert.ThrowsException<KeyNotFoundException>(Action);
+        }
+
+        [TestMethod]
+        public void RemoveBuilding_ExistingBuildingId_BuildingRemoved()
+        {
+            InMemoryBuildingRepository buildingRepository = new InMemoryBuildingRepository();
+
+            BuildingDto building = new BuildingDto() { Id = c_validBuildingId };
+            buildingRepository.AddBuilding(building);
+            buildingRepository.RemoveBuilding(c_validBuildingId);
+
+            Assert.IsFalse(buildingRepository.GetBuildings().Any(building => building.Id == c_validBuildingId));
+        }
+    }
+}
---
+++ Source/Tests/RuneForge.Data.Tests/Units/InMemoryUnitRepositoryTests.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using RuneForge.Data.Units;
+
+namespace RuneForge.Data.Tests.Units
+{
+    [TestClass]
+    public class InMemoryUnitRepositoryTests
+    {
+        private const int c_invalidUnitId = 0;
+        private const int c_validUnitId = 1;
+
+        [TestMethod]
+        public void AddUnit_NullUnitId_ArgumentExceptionThrown()
+        {
+            InMemoryUnitRepository unitRepository = new InMemoryUnitRepository();
+
+            void Action() => unitRepository.AddUnit(new UnitDto() { Id = c_invalidUnitId });
+
+            Assert.ThrowsException<ArgumentException>(Action);
+        }
+
+        [TestMethod]
+        public void AddUnit_ExistingUnitId_InvalidOperationExceptionThrown()
+        {
+            InMemoryUnitRepository unitRepository = new InMemoryUnitRepository();
+
+            unitRepository.AddUnit(new UnitDto() { Id = c_validUnitId });
+            void Action() => unitRepository.AddUnit(new UnitDto() { Id = c_validUnitId });
+
+            Assert.ThrowsException<InvalidOperationException>(Action);
+        }
+
+        [TestMethod]
+        public void AddUnit_NormalUnit_UnitAdded()
+        {
+            InMemoryUnitRepository unitRepository = new InMemoryUnitRepository();
+
+            UnitDto unit = new UnitDto() { Id = c_validUnitId };
+            unitRepository.AddUnit(unit);
+
+            Assert.AreSame(unit, unitRepository.GetUnit(unit.Id));
+        }
+
+        [TestMethod]
+        public void SaveUnit_NonExistingUnitId_KeyNotFoundExceptionThrown()
+        {
+            InMemoryUnitRepository unitRepository = new InMemoryUnitRepository();
+
+            void Action() => unitRepository.SaveUnit(new UnitDto() { Id = c_validUnitId });
+
+            Assert.ThrowsException<KeyNotFoundException>(Action);
+        }
+
+        [TestMethod]
+        public void SaveUnit_SameUnit_NoAction()
+        {
+            InMemoryUnitRepository unitRepository = new InMemoryUnitRepository();
+
+            UnitDto unit = new UnitDto() { Id = c_validUnitId };
+            unitRepository.AddUnit(unit);
+            unitRepository.SaveUnit(unit);
+
+            Assert.AreSame(unit, unitRepository.GetUnit(unit.Id));
+        }
+
+        [TestMethod]
+        public void SaveUnit_UnitWithEqualId_UnitSaved()
+        {
+            InMemoryUnitRepository unitRepository = new InMemoryUnitRepository();
+
+            UnitDto initialUnit = new UnitDto() { Id = c_validUnitId };
+            UnitDto finalUnit = new UnitDto() { Id = c_validUnitId };
+            unitRepository.AddUnit(initialUnit);
+            unitRepository.SaveUnit(finalUnit);
+
+            Assert.AreSame(finalUnit, unitRepository.GetUnit(c_validUnitId));
+        }
+
+        [TestMethod]
+        public void RemoveUnit_NonExistingUnitId_KeyNotFoundExceptionThrown()
+        {
+            InMemoryUnitRepository unitRepository = new InMemoryUnitRepository();
+
+            void Action() => unitRepository.RemoveUnit(c_validUnitId);
+
+            Assert.ThrowsException<KeyNotFoundException>(Action);
+        }
+
+        [TestMethod]
+        public void RemoveUnit_ExistingUnitId_UnitRemoved()
+        {
+            InMemoryUnitRepository unitRepository = new InMemoryUnitRepository();
+
+            UnitDto unit = new UnitDto() { Id = c_validUnitId };
+            unitRepository.AddUnit(unit);
+            unitRepository.RemoveUnit(c_validUnitId);
+
+            Assert.IsFalse(unitRepository.GetUnits().Any(unit => unit.Id == c_validUnitId));
+        }
+    }
+}
